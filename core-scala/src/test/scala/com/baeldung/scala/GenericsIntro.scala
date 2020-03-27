7f7f225a75951d9e2b9cc144fcba5c6c91cd3ef0
Guoneng Zhong <guoneng.list@gmail.com>
2020-03-27 12:27:35 +0100

    SCALA-5: Sample code

---
+++ core-scala/src/test/scala/com/baeldung/scala/GenericsIntro.scala
@@ -0,0 +1,36 @@
+package com.baeldung.scala
+
+object GenericsIntro {
+  case class Apple(name: String)
+
+  def main(args: Array[String]): Unit = {
+    NonGenericWay.run()
+    GenericWay.run()
+  }
+
+  object NonGenericWay{
+    case class MagicHat(magic: AnyRef)
+
+    def run() = {
+      val rabbitHat = MagicHat(Rabbit(2))
+      val rabbit: Rabbit = rabbitHat.magic.asInstanceOf[Rabbit]
+      println(rabbit.cuteness)
+    }
+  }
+
+  object GenericWay{
+    case class MagicHat[A](magic: A)
+
+    def run() = {
+      val rabbitHat = MagicHat[Rabbit](Rabbit(2))
+      val rabbit: Rabbit = rabbitHat.magic
+      println(rabbit.cuteness)
+    }
+  }
+  case class Rabbit(cuteness: Int)
+
+  def middle[A](input: Seq[A]): A = input(input.size / 2)
+
+  def itemsAt[A, B](index: Int, seq1: Seq[A], seq2: Seq[B]): (A, B) = (seq1(index), seq2(index))
+
+}
