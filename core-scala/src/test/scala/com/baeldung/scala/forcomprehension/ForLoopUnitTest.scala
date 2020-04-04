0b3839d079074e70ba3aa9378dd49253a1d71f92
Andrey Filyanin <Andrey.Filyanin@teligent.ru>
2020-04-04 14:14:34 +0300

    AF [SCALA-16] - Fix unit tests  names

--- core-scala/src/test/scala/com/baeldung/scala/forcomprehension/ForLoopUnitTest.scala
+++ core-scala/src/test/scala/com/baeldung/scala/forcomprehension/ForLoopUnitTest.scala
@@ -36,35 +36,35 @@ class ForLoopUnitTest {
   //givenXXX_whenYYY_thenZZZ or whenXXX_thenYYY
 
   @Test
-  def gevenInclusiveRange_whenForLoop_thenIterateOverEachElement (): Unit = {
+  def givenInclusiveRange_whenForLoop_thenIterateOverEachElement (): Unit = {
     forLoopTest.iterateRangeTo(rangeTo)
     val expected = Seq(1, 2, 3)
     assertEquals(expected, forLoopTest.sideEffectResult)
   }
 
   @Test
-  def gevenExclusiveRange_whenForLoop_thenIterateOverEachElement (): Unit = {
+  def givenExclusiveRange_whenForLoop_thenIterateOverEachElement (): Unit = {
     forLoopTest.iterateRangeUntil(rangeUntil)
     val expected = Seq(1, 2)
     assertEquals(expected, forLoopTest.sideEffectResult)
   }
 
   @Test
-  def gevenExclusiveAndInclusiveRange_whenForLoopWithMltipleGenerators_thenCartesianProduct (): Unit = {
+  def givenExclusiveAndInclusiveRange_whenForLoopWithMltipleGenerators_thenCartesianProduct (): Unit = {
     forLoopTest.multipleGenerators(rangeTo, rangeUntil)
     val expected = Seq("1, 1", "1, 2", "2, 1", "2, 2", "3, 1", "3, 2")
     assertEquals(expected, forLoopTest.sideEffectResult)
   }
 
   @Test
-  def gevenCollection_whenForLoop_thenIterateOverEachElement (): Unit = {
+  def givenCollection_whenForLoop_thenIterateOverEachElement (): Unit = {
     forLoopTest.iterateCollection(colors)
     val expected = Seq("R", "G", "B")
     assertEquals(expected, forLoopTest.sideEffectResult)
   }
 
   @Test
-  def gevenCollection_whenForLoopWithMltipleGenerators_thenAllPossibleCombinations (): Unit = {
+  def givenCollection_whenForLoopWithMltipleGenerators_thenAllPossibleCombinations (): Unit = {
     forLoopTest.iterateCollectionWithMultipleGenerators(colors)
     val expected = List("RRR ", "RRG ", "RRB ", "RGR ", "RGG ", "RGB ", "RBR ", "RBG ", "RBB ",
                         "GRR ", "GRG ", "GRB ", "GGR ", "GGG ", "GGB ", "GBR ", "GBG ", "GBB ",
@@ -73,35 +73,35 @@ class ForLoopUnitTest {
   }
 
   @Test
-  def gevenCollection_whenForLoopWithMltipleGeneratorsAndGuards_thenUniqueLettersCombinations (): Unit = {
+  def givenCollection_whenForLoopWithMltipleGeneratorsAndGuards_thenUniqueLettersCombinations (): Unit = {
     forLoopTest.iterateCollectionsWithGuards(colors)
     val expected = List("RGB ", "RBG ", "GRB ", "GBR ", "BRG ", "BGR ")
     assertEquals(expected, forLoopTest.sideEffectResult)
   }
 
   @Test
-  def gevenMap_whenForLoop_thenCollectionOfStrings (): Unit = {
+  def givenMap_whenForLoop_thenCollectionOfStrings (): Unit = {
     forLoopTest.iterateMap(map)
     val expected = List("R is for Red", "G is for Green", "B is for Blue")
     assertEquals(expected, forLoopTest.sideEffectResult)
   }
 
   @Test
-  def gevenMap_whenForLoopWithMltipleGenerators_thenAllCombinationsOfKeyAndValueList (): Unit = {
+  def givenMap_whenForLoopWithMltipleGenerators_thenAllCombinationsOfKeyAndValueList (): Unit = {
     forLoopTest.iterateMapMultipleGenerators(deck)
     val expected = List("A of ♣", "K of ♣", "Q of ♣", "J of ♦", "10 of ♦", "9 of ♥", "8 of ♥", "7 of ♥", "A of ♠", "K of ♠", "J of ♠", "6 of ♠")
     assertEquals(expected, forLoopTest.sideEffectResult)
   }
 
   @Test
-  def gevenCollection_whenForComprehension_thenReturnedCollectionOfStrings (): Unit = {
+  def givenCollection_whenForComprehension_thenReturnedCollectionOfStrings (): Unit = {
     val result = forLoopTest.pureIteration(numbers)
     val expected = List("1 + 1 = 2", "2 + 2 = 4", "3 + 3 = 6")
     assertEquals(expected, result)
   }
 
   @Test
-  def gevenOptionals_whenForComprehensionOrMap_thenReturnedOptional (): Unit = {
+  def givenOptionals_whenForComprehensionOrMap_thenReturnedOptional (): Unit = {
 
     val resultFor = forLoopTest.forComprehensionWithOptionals(someIntValue, someStringValue)
     val resultMap = forLoopTest.mapOptionals(someIntValue, someStringValue)
