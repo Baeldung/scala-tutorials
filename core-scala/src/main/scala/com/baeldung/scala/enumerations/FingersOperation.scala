c54c38ade661e13879636216c0d22022fcbc31c1
ramkumarvenkat <ramkumar.venkat@gmail.com>
2020-04-24 13:12:58 +0530

    Scala enumerations

---
+++ core-scala/src/main/scala/com/baeldung/scala/enumerations/FingersOperation.scala
@@ -0,0 +1,12 @@
+package com.baeldung.scala.enumerations
+
+import Fingers._
+
+class FingersOperation {
+
+    def isShortest(finger: Finger) =
+        Fingers.values.toList.sortBy(_.height).head == finger
+
+    def twoLongest() =
+        Fingers.values.toList.sortBy(_.heightInCms()).takeRight(2)
+}
---
+++ core-scala/src/test/scala/com/baeldung/scala/enumerations/FingersOperationTest.scala
@@ -0,0 +1,44 @@
+package com.baeldung.scala.enumerations
+
+import Fingers._
+import org.junit.Assert.assertTrue
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class FingersOperationTest {
+
+    @Test
+    def givenAFinger_whenIsShortestCalled_thenCorrectValueReturned() = {
+        val operation = new FingersOperation()
+
+        assertTrue(operation.isShortest(Little))
+        assertFalse(operation.isShortest(Index))
+    }
+
+    @Test
+    def givenFingers_whenTwoLongestCalled_thenCorrectValuesReturned() = {
+        val operation = new FingersOperation()
+
+        assertEquals(List(Index, Middle), operation.twoLongest())
+    }
+
+    @Test
+    def givenStringValueOfFinger_whenWithNameCalled_thenCorrectValueReturned() = {
+        assertEquals(Middle, Fingers.withName("The Middle Finger"))
+    }
+
+    @Test
+    def givenAFinger_whenIdAndtoStringCalled_thenCorrectValueReturned() = {
+        assertEquals(6, Thumb.id)
+        assertEquals("Shorty Finger", Little.toString())
+    }
+
+    @Test
+    def givenFingers_whenValuesCalled_thenOrderedValuesReturned() = {
+        assertEquals(
+            List(Index, Middle, Ring, Little, Thumb),
+            Fingers.values.toList,
+        )
+    }
+}
