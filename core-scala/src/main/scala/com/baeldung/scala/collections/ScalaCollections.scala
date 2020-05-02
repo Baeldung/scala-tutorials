c3c2f582b7b845bbd8ce97eb2fd312e916f886b8
Chaitanya Waikar <chaitanyawaikar1993gmail.com>
2020-05-02 15:56:14 +0530

    SCALA-17 Scala collections

---
+++ core-scala/src/main/scala/com/baeldung/scala/collections/ScalaCollections.scala
@@ -0,0 +1,21 @@
+package com.baeldung.scala.collections
+
+object ScalaCollections {
+
+  // Scala List
+  val numbersList: List[Int] = List(1, 2, 3, 4)
+  val emptyList: List[Int] = List()
+
+  val numbersListWithOperator: List[Int] = 1 :: 2 :: 3 :: 4 :: Nil
+  val emptyListWithNil: List[Int] = Nil
+  val x :: xs = numbersList
+
+  // Scala Set
+  val emptySet: Set[Int] = Set()
+  val numbersSet: Set[Int] = Set(1, 2, 3, 4)
+
+  //Scala Map
+  val immutableMap = Map(1 -> "a", 2 -> "b")
+  val mutableMap = collection.mutable.Map(1 -> "a", 2 -> "b")
+
+}
---
+++ core-scala/src/test/scala/com/baeldung/scala/collections/ScalaCollectionsUnitTest.scala
@@ -0,0 +1,85 @@
+package com.baeldung.scala.collections
+
+import org.scalatest.{Matchers, _}
+
+class ScalaCollectionsUnitTest extends FlatSpec with Matchers {
+
+  "Scala list" should "concatenate using ::: operator" in {
+    //given
+    val list1 = List(1, 2)
+    val list2 = List(3, 4)
+
+    //when
+    val res = list1 ::: list2
+
+    //then
+    res shouldBe List(1, 2, 3, 4)
+  }
+
+  it should "create a list will all identical elements using fill method" in {
+    //when and then
+    List.fill(3)(100) shouldBe List(100, 100, 100)
+  }
+
+  it should "reverse should create a reverse all elements of the list" in {
+    //given
+    val list = List(1, 2, 3, 4)
+
+    //when and then
+    list.reverse shouldBe List(4, 3, 2, 1)
+  }
+
+  "Scala set" should "return head of the set when .head method invoked" in {
+    //given
+    val set = Set(1, 2, 3, 4)
+
+    //when and then
+    set.head shouldBe 1
+  }
+
+  it should "return all element of set except head when .tail method invoked" in {
+    //given
+    val set = Set(1, 2, 3, 4)
+
+    //when and then
+    set.tail shouldBe Set(2, 3, 4)
+  }
+
+  "Maps" should "return all keys from the map when .keys method invoked" in {
+    //given
+    val map = Map(1 -> "a", 2 -> "b")
+
+    //when and then
+    map.keys shouldBe Set(1, 2)
+  }
+
+  it should "return all values from the map when .values invoked" in {
+    //given
+    val map = Map(1 -> "a", 2 -> "b")
+
+    //when and then
+    map.values.toList shouldBe List("a", "b")
+  }
+
+  it should "return None if the key is not found in map when .get method invoked" in {
+    //given
+    val map = Map(1 -> "a", 2 -> "b")
+
+    //when
+    val res = map.get(-1)
+
+    //then
+    res.isEmpty shouldBe true
+  }
+
+  it should "return value if the key is found in map using .get method" in {
+    //given
+    val map = Map(1 -> "a", 2 -> "b")
+
+    //when
+    val res = map.get(1)
+
+    //then
+    res shouldBe Some("a")
+  }
+}
