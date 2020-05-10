cc5b9392e2f31b647a5414bb213b547c92590ba6
toninguyen <contact@tuan-consulting.com>
2020-05-10 09:44:50 +0200

    contact@tuan-consulting.com
    initial commit

---
+++ core-scala/src/main/scala/com/baeldung/scala/traitsabstract/TraitVsAbstract.scala
@@ -0,0 +1,66 @@
+package com.baeldung.scala.traitsabstract
+
+object TraitVsAbstract extends App{
+  val firstBicycle: Bicycle = new Bicycle(2,"Abici" )
+  firstBicycle.run()
+  val firstCar: Car = new Car(4,"Audi",true )
+  firstCar.run()
+}
+
+abstract class Vehicle (numberOfTires: Int, brand: String) {
+
+  def run(): Unit =
+    println(
+      "I am a vehicle !"
+    )
+}
+
+class Car (numberOfTires: Int, brand: String, isDiesel: Boolean) extends Vehicle (numberOfTires,brand) with Motor {
+
+  override def run(): Unit = {
+    super.run()
+    println(
+      s"$numberOfTires tires from the brand $brand are running. It is a diesel motor: $isDiesel."
+    )
+    if (isDiesel)
+      println(dieselMessage)
+  }
+}
+
+class Bicycle (numberOfTires: Int, brand: String) extends Vehicle  (numberOfTires,brand) {
+
+  override def run(): Unit = {
+    super.run()
+    println(
+      s"$numberOfTires from the brand $brand are running"
+    )
+  }
+}
+
+trait Motor {
+  def dieselMessage: String =
+    "I am not environment friendly"
+  def noDieselMessage: String =
+    "I am environment friendly"
+}
+
+
+object bmwB38 extends Motor {
+  def run(): Unit = {
+    val noDiesel = noDieselMessage
+    println(
+      s"I am a bmwB38 ! $noDiesel"
+    )
+  }
+}
+
+/* This is NOT possible
+object audiA6 extends Vehicle {
+  def run: Unit = {
+    val noDiesel = noDieselMessage
+    println(
+      s"I am a bmwB38 ! $noDiesel"
+    )
+  }
+}
+ */
