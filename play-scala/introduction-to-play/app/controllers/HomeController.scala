e7be61a76460fcc580312ff749e9dac53e253c16
Bartosz Mikulski <mail@mikulskibartosz.name>
2020-05-22 21:26:22 +0200

    Introduction to the Play Framework in Scala

---
+++ play-scala/introduction-to-play/app/controllers/HomeController.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+@Singleton class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+    def index() = Action { implicit request: Request[AnyContent] =>
+        Ok(views.html.firstexample())
+    }
+
+    def printSum(first: Long, second: Long) = Action { implicit request: Request[AnyContent] =>
+        val sum = first + second
+        Ok(views.html.index(sum))
+    }
+}
