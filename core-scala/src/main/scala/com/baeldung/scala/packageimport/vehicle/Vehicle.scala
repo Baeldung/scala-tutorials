d64dd449422b28eaa11c706a3b465015f7428d1a
toninguyen <contact@tuan-consulting.com>
2020-05-27 15:31:18 +0200

    SCALA-41
    add modules

---
+++ core-scala/src/main/scala/com/baeldung/scala/packageimport/vehicle/Vehicle.scala
@@ -0,0 +1,30 @@
+package com.baeldung.scala.packageimport.vehicle {
+
+  import java.util.{Date => utilDate}
+  import java.sql.{Date => sqlDate}
+
+  abstract class Vehicle(numberOfTires: Int, brand: String) {
+
+    def run(): Unit = {
+      val dt: utilDate = new utilDate()
+      val dtSql: sqlDate = new sqlDate(System.currentTimeMillis())
+      println(s"I am a vehicle running on $dt !")
+      println(s"I am a vehicle running on $dtSql !")
+    }
+  }
+
+}
+
+package com.baeldung.scala.packageimport.vehicleNew {
+
+  import java.sql.{Date => sqlDate}
+
+  abstract class Vehicle(numberOfTires: Int, brand: String) {
+
+    def run(): Unit = {
+      val dtSql: sqlDate = new sqlDate(System.currentTimeMillis())
+      println(s"I am a NEW vehicle running on $dtSql !")
+    }
+  }
+
+}
