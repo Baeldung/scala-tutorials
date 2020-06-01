a69ce72d0d4d5c9d13273175d5ba8cb9609d79be
Luqman Abdul Mushawwir <abdulmushawwir@gmail.com>
2020-06-01 07:04:09 +0700

    Moving all `main` classes to test, along with assertions that illustrate the results

--- lang-scala/src/test/scala/mutability/ImmutabilityCarTest.scala
+++ lang-scala/src/test/scala/mutability/ImmutabilityCarTest.scala
@@ -6,21 +6,19 @@ class ImmutabilityCarTest extends FunSuite {
   test("Mutable vs Immutable variables") {
     val pi = 3.14
     // pi = 4 // Compile error: Reassignment to val
-    println(pi)
 
     var myWeight = 60
-    println(myWeight)
+    assert(myWeight == 60)
     myWeight = 65
-    println(myWeight)
+    assert(myWeight == 65)
   }
 
   test("Immutable car cannot be changed") {
     val myCar = new ImmutabilityCar("blue", 4, "diesel")
     myCar.call()
 
-    // myCar.color = "green" // cannot be accessed from outside the class
-    // myCar.wheels = 5 // reassignment to val
     myCar.engine = "electric"
+    assert(myCar.engine == "electric")
     myCar.call()
   }
 }
