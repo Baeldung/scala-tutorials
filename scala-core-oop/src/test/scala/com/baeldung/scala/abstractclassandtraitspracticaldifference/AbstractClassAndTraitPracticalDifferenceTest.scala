7aeae8196ea59204f4cc8ba2cc4a8b604beba562
Viddyasagar Verma <viddyasagar.verma6@gmail.com>
2020-06-18 17:58:10 +0530

    Reformated.

--- scala-core-oop/src/test/scala/com/baeldung/scala/abstractclassandtraitspracticaldifference/AbstractClassAndTraitPracticalDifferenceTest.scala
+++ scala-core-oop/src/test/scala/com/baeldung/scala/abstractclassandtraitspracticaldifference/AbstractClassAndTraitPracticalDifferenceTest.scala
@@ -4,11 +4,13 @@ import com.baeldung.scala.abstractclassandtraitspracticaldifference.AbstractClas
 import org.scalatest.{FlatSpec, Matchers}
 
 class AbstractClassAndTraitPracticalDifferenceTest extends FlatSpec with Matchers {
+
   "A Audi" should "have a model equal to the passed value" in {
     val audi = Audi("A6")
     audi.model should be("A6")
     audi.modelName() should be(s"Model is ${audi.model}")
   }
+
   "A Vehicle" should "have a model equal to the passed value" in {
     val vehicle = Vehicle("A9")
     vehicle.model should be("A9")
