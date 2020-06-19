181a9aec35c4f510b66901bf4d306fa0895245c4
Nirmalya Sengupta <nirmalya-at-baeldung@gmail.com>
2020-06-20 00:17:57 +0530

    - Added more examples following comments from Greg Martin

--- core-scala/src/test/scala/com/baeldung/scala/dataTypesAnsOps/DatatypesAndOpsTest.scala
+++ core-scala/src/test/scala/com/baeldung/scala/dataTypesAnsOps/DatatypesAndOpsTest.scala
@@ -13,6 +13,8 @@ class DatatypesAndOpsTest extends WordSpec with Matchers  {
       val c3: Char = 126
       val c4: Char = 169
 
+      // val b3: Byte = 169 (will not compile)
+
       b1.isInstanceOf[Byte]       should be (true)
       b1                          should be (0)
       b2.isInstanceOf[Byte]       should be (true)
@@ -22,6 +24,29 @@ class DatatypesAndOpsTest extends WordSpec with Matchers  {
       c3                          should be ('~')
       c4                          should be ('©')
 
+
+      val i1: Int   = 65535
+      val i2: Int   = 65536
+      val l1: Long  = 65536
+
+
+      val i3: Int   = 32768
+      val s1: Short = 32767
+      // val s2: Short = 32768 (will not compile)
+
+      // Types are inferred from the values assigned
+      val i5 = 1234   // inferred as an Int
+      val i6 = 32767  // inferred as an Int
+      val s2: Short = 32767 // qualify with intended type
+      val i7  = 127 // inferred as an Int
+      val b3: Byte = 127    // qualify with intended type
+      val i8 = 0xAFBF // HEX value, inferred as an Int
+      val c5 = 234    // inferred as an Int
+      val c6 ='®'     // inferred as a Char
+      val l4 = 1234L  // 'L' signifies a Long literal
+      val l3 = 0xCAFEBABEL // 'L' signifies a Long literal, even for a HEX value
+
+
       val trueVal = true
       val falseVal = !true
       val falseValOtherWay = !true
@@ -29,12 +54,20 @@ class DatatypesAndOpsTest extends WordSpec with Matchers  {
       trueVal            should be (true)
       falseVal           should be (false)
       falseValOtherWay   should be (false)
+
+      val f1 = 12.05f       // 'f' signifies that it is a Float
+      val d1 = 12.3495067   // inferred as a Double
+      val d2 = 12.3495067D  // 'D' signfies a Double, but is optional
+
+      val name = "Diego Armando Maradona"
+      val nameWithQuote = "Kerry O\'keffey"
+      val ageAsString = "100"
     }
 
     "convert to and from Int types, following the conversion rules " in {
 
        // The line below will not compile!
-       // val justAByte: Byte =  129  // Beyond the range of values allowed for Byte
+       // val justAByte: Byte =  129  // Beyond the range of values allowed for Byte, will not compile
 
        val aByteSizedInteger = 127
        val byteFromInt = aByteSizedInteger.toByte
@@ -51,6 +84,12 @@ class DatatypesAndOpsTest extends WordSpec with Matchers  {
       wrappedByte                                 should be (-128)
       wrappedByteAgain                            should be (-127)
       converedBackToInt                           should be (-127)
+
+      val c4: Char = '®'
+      val symbolToInt = c4.toInt // from a Char
+      val stringToInt = "100".toInt
+
+
 
     }
 
@@ -71,6 +110,17 @@ class DatatypesAndOpsTest extends WordSpec with Matchers  {
        i2          should  equal(i1.toLong)
        i3          should  equal(i1)
        s3          should  be (0)   // Oops! A Short is too small for a large value as 65536
+
+      val i10 = 127       // Integer 127
+      val i11 = 128       // Integer 128
+      i11.isValidByte   should be (true)
+      i10.isValidByte   should be (false)
+
+      val i31 = 65536
+      i31.isValidShort should be (false)
+
+      val d3 = 2E31
+      d3.isValidInt should be (false)
 
     }
 
