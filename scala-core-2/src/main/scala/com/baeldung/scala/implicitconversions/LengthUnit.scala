8cc4f4e7c92963cdac491437226836175a47cad4
lukasz.drygala <lukasz.drygala@gmail.com>
2020-06-29 20:26:29 +0200

    implicit conversions

---
+++ scala-core-2/src/main/scala/com/baeldung/scala/implicitconversions/LengthUnit.scala
@@ -0,0 +1,5 @@
+package com.baeldung.scala.implicitconversions
+
+case class Centimeters(value: Double) extends AnyVal
+case class Meters(value: Double) extends AnyVal
+case class Kilometers(value: Double) extends AnyVal
