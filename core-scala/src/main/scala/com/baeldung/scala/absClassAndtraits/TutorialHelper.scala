6e2ef63f50d6a020f01551e388caa377a256d1d8
Nirmalya Sengupta <nirmalya.at.baeldung@gmail.com>
2020-07-04 22:33:22 +0530

    -- Reformatted code following observations by Greg Martin

--- core-scala/src/main/scala/com/baeldung/scala/absClassAndtraits/TutorialHelper.scala
+++ core-scala/src/main/scala/com/baeldung/scala/absClassAndtraits/TutorialHelper.scala
@@ -1,24 +1,28 @@
 package com.baeldung.scala.absClassAndtraits
 
-
-
 object TutorialHelper {
 
   def countryFinder(place: String): String =
     if (place == "Dublin") "Ireland"
     else if (place == "Bucharest") "Romania"
-    else if (place == "New York")  "USA"
-    else if (place == "Calcutta")  "India"
+    else if (place == "New York") "USA"
+    else if (place == "Calcutta") "India"
     else "Unknown"
 
-  abstract class Person (val name:String, val age: Int, placeOfBirth: String) {
+  abstract class Person(val name: String, val age: Int, placeOfBirth: String) {
     def isASeniorCitizen: Boolean = age >= 60
     def belongsToCountry: String
   }
 
-  class Employee(empCode: Int, name: String, age: Int, placeOfBirth: String,  placeOfWork: String, workingForYears: Int)
-    extends Person(name,age, placeOfBirth) {
-    def isEligibleForSpecialAllowance: Boolean = super.isASeniorCitizen && this.workingForYears > 10
+  class Employee(empCode: Int,
+                 name: String,
+                 age: Int,
+                 placeOfBirth: String,
+                 placeOfWork: String,
+                 workingForYears: Int)
+      extends Person(name, age, placeOfBirth) {
+    def isEligibleForSpecialAllowance: Boolean =
+      super.isASeniorCitizen && this.workingForYears > 10
     def isWorkingAtHQ: Boolean = this.placeOfWork == "HQ"
     override def belongsToCountry: String = countryFinder(this.placeOfBirth)
   }
@@ -30,37 +34,59 @@ object TutorialHelper {
   }
 
   trait CanSing {
-    def requestASong (firstLine: String): String
+    def requestASong(firstLine: String): String
   }
 
   trait CanPlaySoccer {
     def showDribblingSkills(notEarlierThanYears: Int): String // Link to a youtube video
   }
 
-  class BirdLikeEmployee(
-    empCode: Int, name: String, age: Int, placeOfBirth: String,  placeOfWork: String, workingForYears: Int)
-  extends CanFly {
+  class BirdLikeEmployee(empCode: Int,
+                         name: String,
+                         age: Int,
+                         placeOfBirth: String,
+                         placeOfWork: String,
+                         workingForYears: Int)
+      extends CanFly {
     override val minWings: Int = 2
   }
 
-  class SuperWomanLikeEmployee(
-      empCode: Int, name: String, age: Int, placeOfBirth: String,  placeOfWork: String, workingForYears: Int)
-    extends CanFly {
-      override val minWings: Int = 2
-      extraWings = 2
-      override def flyFor(howLong: Int): Int = (minWings * extraWings) * howLong * 10
+  class SuperWomanLikeEmployee(empCode: Int,
+                               name: String,
+                               age: Int,
+                               placeOfBirth: String,
+                               placeOfWork: String,
+                               workingForYears: Int)
+      extends CanFly {
+    override val minWings: Int = 2
+    extraWings = 2
+    override def flyFor(howLong: Int): Int =
+      (minWings * extraWings) * howLong * 10
   }
 
-  class SingerAndFlierEmployee(
-    empCode: Int, name: String, age: Int, placeOfBirth: String,  placeOfWork: String, workingForYears: Int)
-    extends Employee(empCode,name,age, placeOfBirth,placeOfWork, workingForYears) with CanFly with CanSing {
+  class SingerAndFlierEmployee(empCode: Int,
+                               name: String,
+                               age: Int,
+                               placeOfBirth: String,
+                               placeOfWork: String,
+                               workingForYears: Int)
+      extends Employee(
+        empCode,
+        name,
+        age,
+        placeOfBirth,
+        placeOfWork,
+        workingForYears
+      )
+      with CanFly
+      with CanSing {
 
-      override def belongsToCountry: String = countryFinder(this.placeOfBirth)
-      override val minWings: Int = 4
-      extraWings = 2
-      override def requestASong(firstLine: String): String = "www.youtube.com/bundleInOfficeLibrary.mp4"
-      override def flyFor(howLong: Int): Int = (minWings * extraWings) * howLong * 10
+    override def belongsToCountry: String = countryFinder(this.placeOfBirth)
+    override val minWings: Int = 4
+    extraWings = 2
+    override def requestASong(firstLine: String): String =
+      "www.youtube.com/bundleInOfficeLibrary.mp4"
+    override def flyFor(howLong: Int): Int =
+      (minWings * extraWings) * howLong * 10
   }
-
-
 }
