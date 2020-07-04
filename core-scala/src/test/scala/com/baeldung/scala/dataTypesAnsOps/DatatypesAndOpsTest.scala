b81d83c3fa72517f6876d9596363a645e347154f
Nirmalya Sengupta <nirmalya-at-baeldung@gmail.com>
2020-07-04 22:13:49 +0530

    -- Applied Scala Code formatting, following observations made by Greg Martin

--- core-scala/src/test/scala/com/baeldung/scala/dataTypesAnsOps/DatatypesAndOpsTest.scala
+++ core-scala/src/test/scala/com/baeldung/scala/dataTypesAnsOps/DatatypesAndOpsTest.scala
@@ -15,14 +15,14 @@ class DatatypesAndOpsTest extends WordSpec with Matchers  {
 
       // val b3: Byte = 169 (will not compile)
 
-      b1.isInstanceOf[Byte]       should be (true)
-      b1                          should be (0)
-      b2.isInstanceOf[Byte]       should be (true)
-      b2                          should be (127)
-      c1                          should be ('A')
-      c1.isInstanceOf[Char]       should be (true)
-      c3                          should be ('~')
-      c4                          should be ('©')
+      b1.isInstanceOf[Byte] should be(true)
+      b1 should be(0)
+      b2.isInstanceOf[Byte] should be(true)
+      b2 should be(127)
+      c1 should be('A')
+      c1.isInstanceOf[Char] should be(true)
+      c3 should be('~')
+      c4 should be('©')
 
 
       val i1: Int   = 65535
@@ -35,15 +35,15 @@ class DatatypesAndOpsTest extends WordSpec with Matchers  {
       // val s2: Short = 32768 (will not compile)
 
       // Types are inferred from the values assigned
-      val i5 = 1234   // inferred as an Int
-      val i6 = 32767  // inferred as an Int
+      val i5 = 1234 // inferred as an Int
+      val i6 = 32767 // inferred as an Int
       val s2: Short = 32767 // qualify with intended type
-      val i7  = 127 // inferred as an Int
-      val b3: Byte = 127    // qualify with intended type
+      val i7 = 127 // inferred as an Int
+      val b3: Byte = 127 // qualify with intended type
       val i8 = 0xAFBF // HEX value, inferred as an Int
-      val c5 = 234    // inferred as an Int
-      val c6 ='®'     // inferred as a Char
-      val l4 = 1234L  // 'L' signifies a Long literal
+      val c5 = 234 // inferred as an Int
+      val c6 = '®' // inferred as a Char
+      val l4 = 1234L // 'L' signifies a Long literal
       val l3 = 0xCAFEBABEL // 'L' signifies a Long literal, even for a HEX value
 
 
@@ -55,9 +55,9 @@ class DatatypesAndOpsTest extends WordSpec with Matchers  {
       falseVal           should be (false)
       falseValOtherWay   should be (false)
 
-      val f1 = 12.05f       // 'f' signifies that it is a Float
-      val d1 = 12.3495067   // inferred as a Double
-      val d2 = 12.3495067D  // 'D' signfies a Double, but is optional
+      val f1 = 12.05f  // 'f' signifies that it is a Float
+      val d1 = 12.3495067  // inferred as a Double
+      val d2 = 12.3495067D // 'D' signfies a Double, but is optional
 
       val name = "Diego Armando Maradona"
       val nameWithQuote = "Kerry O\'keffey"
@@ -79,48 +79,46 @@ class DatatypesAndOpsTest extends WordSpec with Matchers  {
        val converedBackToInt = convFromInt.toInt
 
 
-      byteFromInt.isInstanceOf[Byte]              should be (true)
-      byteFromInt                                 should be (127)
-      wrappedByte                                 should be (-128)
-      wrappedByteAgain                            should be (-127)
-      converedBackToInt                           should be (-127)
+      byteFromInt.isInstanceOf[Byte] should be(true)
+      byteFromInt should be(127)
+      wrappedByte should be(-128)
+      wrappedByteAgain should be(-127)
+      converedBackToInt should be(-127)
 
       val c4: Char = '®'
       val symbolToInt = c4.toInt // from a Char
       val stringToInt = "100".toInt
 
-
-
     }
 
     "convert to and from Long types, following the conversion rules " in {
 
-       val l     = 65536L   // Long literal
-       val i1    = 65536    // No qualifier, therefore Int
-       val i2    = l        // i2 is a Long because RHS is a Long
+      val l = 65536L // Long literal
+      val i1 = 65536 // No qualifier, therefore Int
+      val i2 = l // i2 is a Long because RHS is a Long
 
-       // The followinig line will not compile because Long, a larger size is being cast into Into, a smaller size
-       // val i2: Int = l    // the target variable's type is explicit: an Int
+      // The followinig line will not compile because Long, a larger size is being cast into Into, a smaller size
+      // val i2: Int = l    // the target variable's type is explicit: an Int
 
-      val i3 = l.toInt     // Conversion helper function works
+      val i3 = l.toInt // Conversion helper function works
       val s3 = l.toShort
 
-       l           should  be (65536L)
-       i1          should  be (65536)
-       i2          should  equal(i1.toLong)
-       i3          should  equal(i1)
-       s3          should  be (0)   // Oops! A Short is too small for a large value as 65536
+      l should be(65536L)
+      i1 should be(65536)
+      i2 should equal(i1.toLong)
+      i3 should equal(i1)
+      s3 should be(0) // Oops! A Short is too small for a large value as 65536
 
-      val i10 = 127       // Integer 127
-      val i11 = 128       // Integer 128
-      i11.isValidByte   should be (true)
-      i10.isValidByte   should be (false)
+      val i10 = 127 // Integer 127
+      val i11 = 128 // Integer 128
+      i11.isValidByte should be(true)
+      i10.isValidByte should be(false)
 
       val i31 = 65536
-      i31.isValidShort should be (false)
+      i31.isValidShort should be(false)
 
       val d3 = 2E31
-      d3.isValidInt should be (false)
+      d3.isValidInt should be(false)
 
     }
 
@@ -128,17 +126,17 @@ class DatatypesAndOpsTest extends WordSpec with Matchers  {
 
       class Article(heading: String, noOfLines: Int)
 
-       val i: Int = 1234
-       val d = 1.2E7
-       val b = false
-       val a1: AnyVal = i       // casting an Int to AnyVal
-       val a2: AnyVal = d       // casting a Double to an AnyVal
-       val a3: AnyVal = b       // casting a Boolean to an AnyVal
-       val article = new Article("Baeldung",2000) // an application class
-       val author = "Eugene"
+      val i: Int = 1234
+      val d = 1.2E7
+      val b = false
+      val a1: AnyVal = i // casting an Int to AnyVal
+      val a2: AnyVal = d // casting a Double to an AnyVal
+      val a3: AnyVal = b // casting a Boolean to an AnyVal
+      val article = new Article("Baeldung", 2000) // an application class
+      val author = "Eugene"
 
-       val parentClass1: AnyRef = article       // casting an User-Defined Object to an AnyRef
-       val parentClass2: AnyRef = author        // casting a String to an AnyRef
+      val parentClass1: AnyRef = article // casting an User-Defined Object to an AnyRef
+      val parentClass2: AnyRef = author // casting a String to an AnyRef
 
     }
 
