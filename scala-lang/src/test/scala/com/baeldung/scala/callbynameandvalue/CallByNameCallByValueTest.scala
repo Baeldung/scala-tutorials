2cf3411c97c3546586e1823546f02522fb355c2f
Viddyasagar Verma <viddyasagar.verma6@gmail.com>
2020-07-06 14:28:09 +0530

    Added dash in by-name and by-value in texts.

--- scala-lang/src/test/scala/com/baeldung/scala/callbynameandvalue/CallByNameCallByValueTest.scala
+++ scala-lang/src/test/scala/com/baeldung/scala/callbynameandvalue/CallByNameCallByValueTest.scala
@@ -8,27 +8,27 @@ import org.scalatest.{FlatSpec, Matchers}
  */
 class CallByNameCallByValueTest extends FlatSpec with Matchers {
 
-  "A call by value" should "have evaluated before passing an argument" in {
+  "A call by-value" should "have evaluated before passing an argument" in {
     getTimeByVal(System.currentTimeMillis()) should be(System.currentTimeMillis())
   }
 
-  "A call by name" should "have evaluated when an argument used inside the function's body" in {
+  "A call by-name" should "have evaluated when an argument used inside the function's body" in {
     getTimeByName(System.currentTimeMillis()) should not be (System.currentTimeMillis())
   }
 
-  "A call by name and call by value" should "have reduced to final value" in {
+  "A call by-name and call by-value" should "have reduced to final value" in {
     addFirst(2 + 5, 7) should be(addFirst(7, 2 + 5))
   }
 
-  "A call by value" should "have produce StackOverflowError when addFirst is invoked with an argument " +
-    "infinite() as a call by value" in {
+  "A call by-value" should "have produce StackOverflowError when addFirst is invoked with an argument " +
+    "infinite() as a call by-value" in {
     assertThrows[StackOverflowError] {
       addFirst(infinite(), 4)
     }
   }
 
-  "A call by name" should "have reduced to final value when addFirst is invoked with an argument infinite() " +
-    "as a call by name" in {
+  "A call by-name" should "have reduced to final value when addFirst is invoked with an argument infinite() " +
+    "as a call by-name" in {
     assert(addFirst(4, infinite()) == 8)
   }
 }
