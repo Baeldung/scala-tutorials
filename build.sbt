bea7158ee4f95f5a2f3a2cef1486404b2b04281d
njacobs5074 <nick@highlandcows.com>
2020-08-09 13:22:14 +0900

    Added PR feedback from Kevin Gilmore

--- build.sbt
+++ build.sbt
@@ -21,7 +21,8 @@ lazy val scala_core_2 = (project in file("scala-core-2"))
     libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % Test,
     libraryDependencies += "org.scalamock" %% "scalamock" % "4.4.0" % Test,
     libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % "test",
-  )
+    libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "2.0.0-M1"
+)
 
 lazy val scala_core_oop = (project in file("scala-core-oop"))
   .settings(
---
+++ scala-core-2/src/test/scala/com/baeldung/scala/typecasts/TypeErasureTest.scala
@@ -0,0 +1,36 @@
+package com.baeldung.scala.typecasts
+
+import org.scalatest.{Matchers, WordSpec}
+
+class TypeErasureTest extends  WordSpec with Matchers {
+
+  "Generic types" should {
+    "be discarded" in {
+
+      val l1 = TypeErasure.convertValuesToList[Int](1, 2, 3)
+      assert(l1.isInstanceOf[List[Int]])
+      assert(l1.isInstanceOf[List[String]])
+
+      val l2 = TypeErasure.convertValuesToList[String]("a", "b", "c")
+      assert(l2.isInstanceOf[List[Int]])
+      assert(l2.isInstanceOf[List[String]])
+    }
+  }
+
+  "Casting" should {
+    "be safe to cast T2 to T1" in {
+      import TypeErasure._
+
+      val t2: T2 = new T2
+      val t1: T1 = t2.asInstanceOf[T1]
+      assert(t2.isInstanceOf[T1])
+    }
+
+    "fail to cast T3 to T1" in {
+      import TypeErasure._
+
+      val t3 = new T3
+      assert(!t3.isInstanceOf[T1])
+    }
+  }
+}
