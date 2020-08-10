6801e4635cee4abeccf39fa2af8862017311408a
rcardin <riccardo.cardin@gmail.com>
2020-08-10 15:39:11 +0200

    [SCALA-97] Added code that shows the tell pattern in akka. Updated the version of the Akka library. Added mock tests

---
+++ scala-akka/src/test/scala/com/baeldung/scala/akka/fireandforget/LoggingApplicationUnitTest.scala
@@ -0,0 +1,18 @@
+package com.baeldung.scala.akka.fireandforget
+
+import org.scalatest.FlatSpec
+
+class LoggingApplicationUnitTest extends FlatSpec {
+
+  "A LogKeeperActor" should "log received messages using the trace level" in {
+
+  }
+
+  it should "log received messages using the info level" in {
+
+  }
+
+  it should "log received messages using the error level" in {
+
+  }
+}
