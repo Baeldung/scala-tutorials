546eb7e5d0e3048a1e002bfd2dd9c11f9ac60d00
kayvan Kazeminejad <kayvan@q2io.com>
2020-08-18 07:52:35 -0700

    Fix - removed non relevant files

--- scala-core-fp/src/main/scala/com/baeldung/scala/functor/Tree.scala
+++
@@ -1,7 +0,0 @@
-package com.baeldung.scala.functor
-
-object Tree {
-  sealed abstract class Tree[+A]
-  case class Leaf[A](value: A) extends Tree[A]
-  case class Node[A](left: Tree[A], right: Tree[A]) extends Tree[A] 
-}
