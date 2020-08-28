a3c09e56ae3afb300302b2330d8f2fd2585e394d
Bartosz Mikulski <mail@mikulskibartosz.name>
2020-08-28 16:01:31 +0200

    Indent with 2 spaces

--- play-scala/rest-api/app/controllers/TodoListController.scala
+++ play-scala/rest-api/app/controllers/TodoListController.scala
@@ -8,61 +8,61 @@ import scala.collection.mutable
 
 @Singleton
 class TodoListController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-    private val todoList = new mutable.ListBuffer[TodoListItem]()
-    todoList += TodoListItem(1, "test", true)
-    todoList += TodoListItem(2, "some other value", false)
+  private val todoList = new mutable.ListBuffer[TodoListItem]()
+  todoList += TodoListItem(1, "test", true)
+  todoList += TodoListItem(2, "some other value", false)
 
-    implicit val todoListJson = Json.format[TodoListItem]
-    implicit val newTodoListJson = Json.format[NewTodoListItem]
+  implicit val todoListJson = Json.format[TodoListItem]
+  implicit val newTodoListJson = Json.format[NewTodoListItem]
 
-    // curl localhost:9000/todo
-    def getAll(): Action[AnyContent] = Action {
-        if(todoList.isEmpty) NoContent else Ok(Json.toJson(todoList))
+  // curl localhost:9000/todo
+  def getAll(): Action[AnyContent] = Action {
+    if (todoList.isEmpty) NoContent else Ok(Json.toJson(todoList))
+  }
+
+  // curl localhost:9000/todo/1
+  def getById(itemId: Long) = Action {
+    val foundItem = todoList.find(_.id == itemId)
+    foundItem match {
+      case Some(item) => Ok(Json.toJson(item))
+      case None => NotFound
     }
+  }
 
-    // curl localhost:9000/todo/1
-    def getById(itemId: Long) = Action {
-        val foundItem = todoList.find(_.id == itemId)
-        foundItem match {
-            case Some(item) => Ok(Json.toJson(item))
-            case None => NotFound
-        }
+  // curl -X PUT localhost:9000/todo/done/1
+  def markAsDone(itemId: Long) = Action {
+    val foundItem = todoList.find(_.id == itemId)
+    foundItem match {
+      case Some(item) =>
+        val newItem = item.copy(isItDone = true)
+        todoList.dropWhileInPlace(_.id == itemId)
+        todoList += newItem
+        Accepted(Json.toJson(newItem))
+      case None => NotFound
     }
+  }
 
-    // curl -X PUT localhost:9000/todo/done/1
-    def markAsDone(itemId: Long) = Action {
-        val foundItem = todoList.find(_.id == itemId)
-        foundItem match {
-            case Some(item) =>
-                val newItem = item.copy(isItDone = true)
-                todoList.dropWhileInPlace(_.id == itemId)
-                todoList += newItem
-                Accepted(Json.toJson(newItem))
-            case None => NotFound
-        }
+  // curl -X DELETE localhost:9000/todo/done
+  def deleteAllDone() = Action {
+    todoList.filterInPlace(_.isItDone == false)
+    Accepted
+  }
+
+  // curl -v -d '{"description": "some new item"}' -H 'Content-Type: application/json' -X POST localhost:9000/todo
+  def addNewItem() = Action { implicit request =>
+    val content = request.body
+    val jsonObject = content.asJson
+
+    val todoListItem: Option[NewTodoListItem] = jsonObject.flatMap(Json.fromJson[NewTodoListItem](_).asOpt)
+
+    todoListItem match {
+      case Some(newItem) =>
+        val nextId = todoList.map(_.id).max + 1
+        val toBeAdded = TodoListItem(nextId, newItem.description, false)
+        todoList += toBeAdded
+        Created(Json.toJson(toBeAdded))
+      case None =>
+        BadRequest
     }
-
-    // curl -X DELETE localhost:9000/todo/done
-    def deleteAllDone() = Action {
-        todoList.filterInPlace(_.isItDone == false)
-        Accepted
-    }
-
-    // curl -v -d '{"description": "some new item"}' -H 'Content-Type: application/json' -X POST localhost:9000/todo
-    def addNewItem() = Action { implicit request =>
-        val content = request.body
-        val jsonObject = content.asJson
-
-        val todoListItem: Option[NewTodoListItem] = jsonObject.flatMap(Json.fromJson[NewTodoListItem](_).asOpt)
-
-        todoListItem match {
-            case Some(newItem) =>
-                val nextId = todoList.map(_.id).max + 1
-                val toBeAdded = TodoListItem(nextId, newItem.description, false)
-                todoList += toBeAdded
-                Created(Json.toJson(toBeAdded))
-            case None =>
-                BadRequest
-        }
-    }
+  }
 }
