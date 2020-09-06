08fda6e7de56da7ea229777e1250e11985a38987
remisharoon <remisharoon@gmail.com>
2020-09-06 22:30:28 +0400

    MultipleChoice example sealed trait

--- scala-core/src/main/scala/com/baeldung/scala/sealedkeyword/SealedClassExample.scala
+++ scala-core/src/main/scala/com/baeldung/scala/sealedkeyword/SealedClassExample.scala
@@ -1,5 +1,24 @@
 package com.baeldung.scala.sealedkeyword
 
-class SealedClassExample {
+sealed abstract class MultipleChoice
+
+case class OptionA() extends MultipleChoice
+case class OptionB() extends MultipleChoice
+case class OptionC() extends MultipleChoice
+
+object MultipleChoiceExam extends App {
+
+  def selectOption(option: MultipleChoice): String = option match {
+    case optionA: OptionA => "Option-A Selected"
+    case optionB: OptionB => "Option-B Selected"
+  }
+  /*
+  Warning:(11, 54) match may not be exhaustive.
+  It would fail on the following input: OptionC()
+    def selectOption(option: MultipleChoice): String = option match {
+   */
+
+  println(selectOption(OptionA()))
+  println(selectOption(OptionB()))
 
 }
