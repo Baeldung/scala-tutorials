cf69e7bb73dcd4c4cf6998cd8682d1b4c031a227
njacobs5074 <nick@highlandcows.com>
2020-09-07 10:36:10 +0900

    PR feedback

--- scala-core-2/src/test/scala/com/baeldung/scala/options/OptionUnitTest.scala
+++ scala-core-2/src/test/scala/com/baeldung/scala/options/OptionUnitTest.scala
@@ -54,4 +54,26 @@ class OptionUnitTest extends WordSpec with Matchers {
       assert(OptionExample.whoHasTopScoringTeam(player3, player4, tournament).isEmpty)
     }
   }
+
+  "Option Examples" should {
+    "Successfully map" in {
+      val o1: Option[Int] = Some(10)
+      assert(o1.map(_.toString).contains("10"))
+      assert(o1.map(_ * 2.0).contains(20))
+
+      val o2: Option[Int] = None
+      assert(o2.map(_.toString).isEmpty)
+    }
+
+    "Demonstrate flow control" in {
+      val o1: Option[Int] = Some(10)
+      val o2: Option[Int] = None
+
+      def times2(n: Int): Int = n * 2
+
+      assert(o1.map(times2).contains(20))
+      o2.map(times2)
+      assert(o2.map(times2).isEmpty)
+    }
+  }
 }
