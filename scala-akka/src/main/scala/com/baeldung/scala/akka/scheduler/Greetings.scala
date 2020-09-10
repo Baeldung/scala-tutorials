e5227e4d0730745920c9fe2992445fa155ce0801
Yadu Krishnan <yadavan88@gmail.com>
2020-09-10 11:01:36 +0530

    Akka Scheduler Samples (#215)
    Co-authored-by: Yadukrishnan <yadu.k@reactore.com>

---
+++ scala-akka/src/main/scala/com/baeldung/scala/akka/scheduler/Greetings.scala
@@ -0,0 +1,13 @@
+package com.baeldung.scala.akka.scheduler
+
+import akka.actor.Actor
+
+final case class Greet(to: String, by: String)
+final case class Greeted(msg: String)
+
+class Greetings extends Actor {
+  override def receive: Receive = {
+    case greet: Greet =>
+      sender ! Greeted(s"${greet.by}: Hello, ${greet.to}")
+  }
+}
