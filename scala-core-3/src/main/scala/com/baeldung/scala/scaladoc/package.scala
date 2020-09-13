d0ea79d82e2274f568eb3f7eccd07f0ec87e8ae8
Luqman Abdul Mushawwir <abdulmushawwir@gmail.com>
2020-09-13 22:27:12 +0700

    [SCALA-163] Guide to Scaladocs

---
+++ scala-core-3/src/main/scala/com/baeldung/scala/scaladoc/package.scala
@@ -0,0 +1,8 @@
+package com.baeldung.scala
+
+/** Package for scaladoc tutorial.
+  * Provides examples of scaladoc and their elements such as tags and formattings.
+  *
+  * Class implemented in this package is [[com.baeldung.scala.scaladoc.IntervalTimer]].
+  */
+package object scaladoc {}
