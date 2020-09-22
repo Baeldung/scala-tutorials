1483f7757737b154aeef5b7081481dc2a4be42c3
njacobs5074 <nick@highlandcows.com>
2020-09-22 13:42:04 +0900

    Additional refinements including adding a date parsing example.

--- play-scala/configuration-access/app/services/MyService.scala
+++ play-scala/configuration-access/app/services/MyService.scala
@@ -1,15 +1,25 @@
 package services
 
+import java.util.Date
+
 import com.typesafe.config.Config
 import javax.inject.Inject
 import play.api.{ConfigLoader, Configuration}
 
 import scala.util.Try
 
+object ISO8601DateConfigLoader {
+  implicit val iso8601DateConfigLoader: ConfigLoader[Date] = {
+    ConfigLoader(_.getString)
+      .map[Date](javax.xml.bind.DatatypeConverter.parseDateTime(_).getTime)
+  }
+}
+
 case class PlayerInfo(
     name: String,
     email: String,
     age: Int,
+    signUpDate: Date,
     twitterHandle: Option[String] = None
 )
 
@@ -21,6 +31,9 @@ object PlayerInfo {
         config.getString("name"),
         config.getString("email"),
         config.getInt("age"),
+        javax.xml.bind.DatatypeConverter
+          .parseDateTime(config.getString("signUpDate"))
+          .getTime,
         Try(config.getString("twitterHandle")).toOption
       )
     }
@@ -33,6 +46,9 @@ class MyService @Inject() (configuration: Configuration) {
       configuration.get[String]("player.name"),
       configuration.get[String]("player.email"),
       configuration.get[Int]("player.age"),
+      configuration.get[Date]("player.signUpDate")(
+        ISO8601DateConfigLoader.iso8601DateConfigLoader
+      ),
       configuration.get[Option[String]]("player.twitterHandle")
     )
   }
--- play-scala/configuration-access/test/services/MyServiceSpec.scala
+++ play-scala/configuration-access/test/services/MyServiceSpec.scala
@@ -1,9 +1,10 @@
 package services
 
+import java.util.{Calendar, Date, TimeZone}
+
 import org.mockito.MockitoSugar
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
-import play.api.test.Helpers._
 import play.api.test._
 
 class MyServiceSpec
@@ -11,19 +12,39 @@ class MyServiceSpec
     with GuiceOneAppPerTest
     with Injecting
     with MockitoSugar {
+
+  val signUpDate: Date = {
+    val c: Calendar = Calendar.getInstance()
+    c.set(2020, 8, 22, 10, 10, 6)
+    c.set(Calendar.MILLISECOND, 0)
+    c.getTime
+  }
   "MyService" should {
 
-    "Return the correct PlayerInfo information" in {
+    "Return the correct PlayerInfo information using individual gets" in {
       val myService = inject[MyService]
 
       val playerInfo = PlayerInfo(
         "Player 1",
         "player1@hiddencodex.com",
         18,
+        signUpDate,
         None
       )
       myService.getPlayerInfoVersion1 mustBe playerInfo
-      myService.getPlayerInfoVersion2 mustBe playerInfo
     }
+  }
+
+  "Return the correct PlayerInfo information using ConfigLoader" in {
+    val myService = inject[MyService]
+
+    val playerInfo = PlayerInfo(
+      "Player 1",
+      "player1@hiddencodex.com",
+      18,
+      signUpDate,
+      None
+    )
+    myService.getPlayerInfoVersion2 mustBe playerInfo
   }
 }
