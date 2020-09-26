7f5ea3c4a2cd9b65489bd1c528412417c00b39d5
remisharoon <remisharoon@users.noreply.github.com>
2020-09-26 09:52:29 +0400

    Scala 114 - Example files of article "Sealed in Scala" (#221)
    * sealed class example
    
    * Scala Sealed as ADT example
    
    * MultipleChoice example sealed trait
    
    * sealed as a alternative for Enum
    
    * example shows using sealed classes in another file
    
    * formatting code
    
    * Update scala-core/src/main/scala/com/baeldung/scala/sealedkeyword/AlgebraicDataType.scala
    
    Co-authored-by: Grzegorz Piwowarek <gpiwowarek@gmail.com>

---
+++ scala-core/src/main/scala/com/baeldung/scala/sealedkeyword/SealedClassExample.scala
@@ -0,0 +1,26 @@
+package com.baeldung.scala.sealedkeyword
+
+sealed abstract class MultipleChoice
+
+case class OptionA() extends MultipleChoice
+case class OptionB() extends MultipleChoice
+case class OptionC() extends MultipleChoice
+
+class OptionX() extends MultipleChoice
+
+object MultipleChoiceExam extends App {
+
+  def selectOption(option: MultipleChoice): String = option match {
+    case optionA: OptionA => "Option-A Selected"
+    case optionB: OptionB => "Option-B Selected"
+  }
+  /*
+  Warning:(11, 54) match may not be exhaustive.
+  It would fail on the following input: OptionC()
+    def selectOption(option: MultipleChoice): String = option match {
+   */
+
+  println(selectOption(OptionA()))
+  println(selectOption(OptionB()))
+
+}
