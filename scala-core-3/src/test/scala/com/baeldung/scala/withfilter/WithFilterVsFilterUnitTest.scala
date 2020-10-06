482d85c5b117656a4c0a62a876ddbbd594443a97
lukasz.drygala <lukasz.drygala@gmail.com>
2020-10-06 19:47:32 +0200

    fixes after review

--- scala-core-3/src/test/scala/com/baeldung/scala/withfilter/WithFilterVsFilterUnitTest.scala
+++ scala-core-3/src/test/scala/com/baeldung/scala/withfilter/WithFilterVsFilterUnitTest.scala
@@ -8,39 +8,79 @@ import scala.collection.generic.FilterMonadic
 
 class WithFilterVsFilterUnitTest extends WordSpec with Matchers {
 
+  sealed trait Level
+  object Level {
+    case object Junior extends Level
+    case object Mid extends Level
+    case object Senior extends Level
+  }
+  case class Programmer(name: String,
+                        level: Level,
+                        knownLanguages: List[String])
+
+  val programmers: List[Programmer] = List(
+    Programmer(name = "Kelly",
+               level = Level.Mid,
+               knownLanguages = List("JavaScript")),
+    Programmer(name = "John",
+               level = Level.Senior,
+               knownLanguages = List("Java", "Scala", "Kotlin")),
+    Programmer(name = "Dave",
+               level = Level.Junior,
+               knownLanguages = List("C", "C++"))
+  )
+
+  def isMidOrSenior(implicit counter: AtomicInteger): Programmer => Boolean =
+    programmer => {
+      counter.incrementAndGet()
+      println("verify level " + programmer)
+      List(Level.Mid, Level.Senior).contains(programmer.level)
+    }
+
+  def knowMoreThan1Language(
+      implicit counter: AtomicInteger): Programmer => Boolean =
+    programmer => {
+      counter.incrementAndGet()
+      println("verify number of known languages " + programmer)
+      programmer.knownLanguages.size > 1
+    }
+
+  val getName: Programmer => String =
+    programmer => {
+      println("get name " + programmer)
+      programmer.name
+    }
+
   "Filter" should {
-    "evaluated collection eagerly" in {
-      val counter = new AtomicInteger(0)
-      val isEven: Int => Boolean = n => {
-        counter.getAndIncrement()
-        n % 2 == 0
-      }
-      val numbers: List[Int] = List(1, 2, 3, 4)
+    "filter programmers" in {
+      implicit val counter: AtomicInteger = new AtomicInteger(0)
 
-      val evenNumbers: List[Int] = numbers.filter(isEven)
+      val desiredProgrammers: List[Programmer] = programmers
+        .filter(isMidOrSenior)
+        .filter(knowMoreThan1Language)
 
-      evenNumbers shouldBe List(2, 4)
-      counter.get() shouldBe 4
+      counter.get() shouldBe 5
+
+      desiredProgrammers.map(getName) shouldBe List("John")
     }
   }
 
   "WithFilter" should {
-    "evaluated collection lazily" in {
-      val counter = new AtomicInteger(0)
-      val isEven: Int => Boolean = n => {
-        counter.getAndIncrement()
-        n % 2 == 0
-      }
-      val numbers: List[Int] = List(1, 2, 3, 4)
 
-      val evenNumbersFilter: FilterMonadic[Int, List[Int]] =
-        numbers.withFilter(isEven)
+    "filter programmers" in {
+      implicit val counter: AtomicInteger = new AtomicInteger(0)
+
+      val desiredProgrammers: FilterMonadic[Programmer, List[Programmer]] =
+        programmers
+          .withFilter(isMidOrSenior)
+          .withFilter(knowMoreThan1Language)
+
       counter.get() shouldBe 0
 
-      val evenNumbers: List[Int] = evenNumbersFilter.map(n => n)
-      evenNumbers shouldBe List(2, 4)
-      counter.get() shouldBe 4
+      desiredProgrammers.map(getName) shouldBe List("John")
+      counter.get() shouldBe 5
     }
+
   }
 
 }
