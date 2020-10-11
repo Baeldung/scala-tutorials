54663eff63fd0d98c95fc30bca5adc3717497c2e
ramiz <remisharoon@gmail.com>
2020-10-11 14:42:48 +0400

    Difference between Nil, Null, Nothing, Unit, and None in Scala

--- scala-lang/src/test/scala/com/baeldung/scala/voidtypes/NoneEmptyOptionTest.scala
+++ scala-lang/src/test/scala/com/baeldung/scala/voidtypes/NoneEmptyOptionTest.scala
@@ -3,7 +3,7 @@ package com.baeldung.scala.voidtypes
 import org.scalatest.funsuite.AnyFunSuite
 
 class NoneEmptyOptionTest extends AnyFunSuite{
-  test("  "){
+  test("test checking option return types SOme and None"){
     val studentRegister:Map[Int,String] = Map(1 -> "Berlin", 2 -> "Nairobi")
     assert(NoneEmptyOption.getStudentName(studentRegister, 1) == Some("Berlin"))
     assert(NoneEmptyOption.getStudentName(studentRegister, 0) == None)
