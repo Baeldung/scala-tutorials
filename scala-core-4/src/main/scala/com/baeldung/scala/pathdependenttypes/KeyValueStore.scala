aa679a286550b667904cf72f7e63da0c34c625e5
Milad Khajavi <khajavi@gmail.com>
2020-12-24 10:55:43 +0330

    add Operations trait.

--- scala-core-4/src/main/scala/com/baeldung/scala/pathdependenttypes/KeyValueStore.scala
+++ scala-core-4/src/main/scala/com/baeldung/scala/pathdependenttypes/KeyValueStore.scala
@@ -8,16 +8,19 @@ abstract class Key(val name: String) {
   type ValueType
 }
 
-case class Database() {
+trait Operations {
+  def set(key: Key)(v: key.ValueType)(implicit enc: Encoder[key.ValueType]): Unit
+  def get(key: Key)(implicit decoder: Decoder[key.ValueType]): Option[key.ValueType]
+}
+
+case class Database() extends Operations {
   private val db = mutable.Map.empty[String, Array[Byte]]
 
-  def set(k: Key)(v: k.ValueType)(implicit enc: Encoder[k.ValueType]): Unit =
-    db.update(k.name, enc.encode(v))
+  def set(key: Key)(value: key.ValueType)(implicit enc: Encoder[key.ValueType]): Unit =
+    db.update(key.name, enc.encode(value))
 
-  def get(
-    k: Key
-  )(implicit decoder: Decoder[k.ValueType]): Option[k.ValueType] = {
-    db.get(k.name).map(x => decoder.encode(x))
+  def get(key: Key)(implicit decoder: Decoder[key.ValueType]): Option[key.ValueType] = {
+    db.get(key.name).map(x => decoder.encode(x))
   }
 
 }
