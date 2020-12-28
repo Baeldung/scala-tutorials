2c69c531dc20acf03ca16c3e6b344ca6f6e1b12e
remis <remisharoon@gmail.com>
2020-12-28 21:21:51 +0400

    abstracting the types using generic types

--- scala-core-4/src/main/scala/com/baeldung/scala/uniontypes/ArbitraryArityUnionType.scala
+++ scala-core-4/src/main/scala/com/baeldung/scala/uniontypes/ArbitraryArityUnionType.scala
@@ -8,17 +8,28 @@ object ArbitraryArityUnionType extends App {
     case b: Boolean => "%b is a Boolean".format(b)
   }
 
-  implicit val intInstance: IntOrStringOrBool[Int] =
-    new IntOrStringOrBool[Int] {}
-  implicit val strInstance: IntOrStringOrBool[String] =
-    new IntOrStringOrBool[String] {}
-  implicit val boolInstance: IntOrStringOrBool[Boolean] =
-    new IntOrStringOrBool[Boolean] {}
-
   sealed trait IntOrStringOrBool[T]
+  object IntOrStringOrBool {
+    implicit val intInstance: IntOrStringOrBool[Int] = new IntOrStringOrBool[Int] {}
+    implicit val strInstance: IntOrStringOrBool[String] = new IntOrStringOrBool[String] {}
+    implicit val boolInstance: IntOrStringOrBool[Boolean] = new IntOrStringOrBool[Boolean] {}
+  }
+  println(isIntOrStringOrBool(10))  //prints "10 is an Integer"
+  println(isIntOrStringOrBool("hello")) // prints "hello is a String"
+  println(isIntOrStringOrBool(true))  // prints "true is a Boolean"
 
-  println(isIntOrStringOrBool(10))
-  println(isIntOrStringOrBool("hello"))
-  println(isIntOrStringOrBool(true))
+  sealed trait AOrB[A, B]
+  object AOrB {
+    implicit def aInstance[A,B](a: A) = new AOrB[A, B] {}
+    implicit def bInstance[A,B](b: B) = new AOrB[A, B] {}
+  }
+
+  def isIntOrString[T <% String AOrB Int](t: T): String = t match {
+    case i: Int => "%d is an Integer".format(i)
+    case s: String => "%s is a String".format(s)
+  }
+
+  println(isIntOrString(10))  //prints "10 is an Integer"
+  println(isIntOrString("hello")) // prints "hello is a String"
 
 }
--- scala-core-4/src/test/scala/com/baeldung/scala/uniontypes/ArbitraryArityUnionTypeTest.scala
+++ scala-core-4/src/test/scala/com/baeldung/scala/uniontypes/ArbitraryArityUnionTypeTest.scala
@@ -16,4 +16,11 @@ class ArbitraryArityUnionTypeTest extends FlatSpec with Matchers {
     isIntOrStringOrBool(true) shouldEqual "true is a Boolean"
   }
 
+  "isIntOrString" should "be able to take an integer parameter" in {
+    isIntOrString(10) shouldEqual "10 is an Integer"
+  }
+
+  "isIntOrString" should "be able to take an string parameter" in {
+    isIntOrString("hello") shouldEqual "hello is a String"
+  }
 }
