4ccf574e9b4afeee8ab6cbf8544ffbdd047bf782
Bartosz Mikulski <mail@mikulskibartosz.name>
2021-01-13 16:47:19 +0100

    custom error handling in Play Framework

---
+++ play-scala/custom-error-handling/app/controllers/ErrorDemoController.scala
@@ -0,0 +1,28 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc.{BaseController, ControllerComponents, Action, AnyContent}
+
+@Singleton
+class ErrorDemoController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+    def noError(): Action[AnyContent] = Action {
+        Ok
+    }
+
+    def exception(): Action[AnyContent] = Action {
+        throw new RuntimeException("Pretend that we have an application error.")
+        Ok // I add this line just to make the types match
+    }
+
+    def internalError(): Action[AnyContent] = Action {
+        InternalServerError
+    }
+
+    def notFound(): Action[AnyContent] = Action {
+        NotFound
+    }
+
+    def badRequest(): Action[AnyContent] = Action {
+        BadRequest
+    }
+}
---
+++ play-scala/custom-error-handling/app/errors/CustomErrorHandler.scala
@@ -0,0 +1,28 @@
+package errors
+
+import controllers.routes
+import play.api.http.HttpErrorHandler
+import play.api.mvc._
+import play.api.http.Status._
+import play.api.mvc.Results._
+import scala.concurrent._
+import javax.inject.Singleton
+
+@Singleton
+class CustomErrorHandler extends HttpErrorHandler {
+    def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
+        if (statusCode == NOT_FOUND) { // works only when we access a not existing page, not when we return NotFound on purpose
+            Future.successful(Redirect(routes.ErrorDemoController.noError()))
+        } else {
+            Future.successful(
+                Status(statusCode)("A client error occurred.")
+            )
+        }
+    }
+
+    def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
+        Future.successful(
+            InternalServerError("A server error occurred: " + exception.getMessage)
+        )
+    }
+}
