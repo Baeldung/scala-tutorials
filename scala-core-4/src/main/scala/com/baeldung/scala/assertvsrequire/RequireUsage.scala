f70152fc4c6a11ccc0dcd0b0505703544b924ec1
remisharoon <remisharoon@users.noreply.github.com>
2021-02-10 10:36:51 +0400

    Scala 152 - assert vs require in Scala (#273)
    * example code for SCALA-152 differences between assert vs require in Scala
    
    * fixing typos in variable names

---
+++ scala-core-4/src/main/scala/com/baeldung/scala/assertvsrequire/RequireUsage.scala
@@ -0,0 +1,18 @@
+package com.baeldung.scala.assertvsrequire
+
+object RequireUsage extends App {
+
+  def issueDrivingLicense(name: String, age: Int): Unit = {
+    require(age >= 18)
+    println("Issued Driving License to %s ".format(name))
+  }
+  issueDrivingLicense("Darwin", 38) // prints "Issued Driving License to Darwin"
+
+  try {
+    issueDrivingLicense("Jr. Darwin", 5) // prints "Failed in require precondition"
+  } catch {
+    case e: IllegalArgumentException =>
+      println("Failed in require precondition")
+  }
+
+}
