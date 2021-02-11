44bbef3806e1f3a9bd13ab1a7b793626d02ff890
mansibabbar <mansib@carbyne911.com>
2021-02-11 10:22:59 +0530

    Code refactored

--- scala-core-fp/src/test/scala/com/baeldung/scala/firstclassfunctions/PartiallyAppliedFunctionSpec.scala
+++ scala-core-fp/src/test/scala/com/baeldung/scala/firstclassfunctions/PartiallyAppliedFunctionSpec.scala
@@ -7,7 +7,7 @@ class PartiallyAppliedFunctionSpec extends AnyFlatSpec with Matchers
 {
     "Partially applied function" should "calculate selling price after discount" in
     {
-        val discountApplied = PartiallyAppliedFunction.calculateSellingPrice(25, _)
+        val discountApplied = PartiallyAppliedFunction.calculateSellingPrice(25, _ :Double)
         val sellingPrice = discountApplied(1000)
         assert(sellingPrice == 750)
     }
