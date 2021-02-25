de0b639640e3739b451c3c9747198195a146b60e
rcardin <riccardo.cardin@gmail.com>
2021-02-25 21:57:41 +0100

    [SCALA-181] Added different kinds of dependency resolution

--- scala-core-fp/src/main/scala/com/baeldung/scala/tagless/TaglessFinal.scala
+++ scala-core-fp/src/main/scala/com/baeldung/scala/tagless/TaglessFinal.scala
@@ -13,13 +13,17 @@ object TaglessFinal {
 
   trait ShoppingCarts[F[_]] {
     def create(id: String): F[Unit]
+
     def find(id: String): F[Option[ShoppingCart]]
+
     def add(sc: ShoppingCart, product: Product): F[ShoppingCart]
   }
 
   object ShoppingCarts {
     type ShoppingCartRepository = Map[String, ShoppingCart]
     type ScRepoState[A] = State[ShoppingCartRepository, A]
+
+    def apply[F[_]](implicit sc: ShoppingCarts[F]): ShoppingCarts[F] = sc
 
     implicit object TestShoppingCartInterpreter extends ShoppingCarts[ScRepoState] {
       override def create(id: String): ScRepoState[Unit] =
@@ -40,11 +44,31 @@ object TaglessFinal {
           (carts + (sc.id -> updatedCart), updatedCart)
         }
     }
+
+    class ShoppingCartWithDependencyInterpreter private(repo: ShoppingCartRepository)
+      extends ShoppingCarts[ScRepoState] {
+      override def create(id: String): ScRepoState[Unit] = ???
+
+      override def find(id: String): ScRepoState[Option[ShoppingCart]] = ???
+
+      override def add(sc: ShoppingCart, product: Product): ScRepoState[ShoppingCart] = ???
+    }
+
+    object ShoppingCartWithDependencyInterpreter {
+      def make(): ShoppingCartWithDependencyInterpreter = {
+        new ShoppingCartWithDependencyInterpreter(repository)
+      }
+
+      private val repository: ShoppingCartRepository = Map()
+    }
+
   }
 
   object Program {
-    def createAndAddToCart[F[_]: Monad](product: Product, cartId: String)
-                              (implicit shoppingCarts: ShoppingCarts[F]): F[ShoppingCart] =
+
+    // Using implicit objects (explicit)
+    def createAndAddToCart[F[_] : Monad](product: Product, cartId: String)
+                                        (implicit shoppingCarts: ShoppingCarts[F]): F[ShoppingCart] =
       for {
         _ <- shoppingCarts.create(cartId)
         maybeSc <- shoppingCarts.find(cartId)
@@ -54,5 +78,33 @@ object TaglessFinal {
           case _ => maybeSc
         }
       }
+
+    // Using the summoned values pattern
+    def createAndToCart[F[_] : Monad : ShoppingCarts](product: Product, cartId: String): Unit = {
+      for {
+        _ <- ShoppingCarts[F].create(cartId)
+        maybeSc <- ShoppingCarts[F].find(cartId)
+      } yield {
+        maybeSc match {
+          case Some(sc) => ShoppingCarts[F].add(sc, product)
+          case _ => maybeSc
+        }
+      }
+    }
   }
+
+  class ProgramWithDep[F[_] : Monad](carts: ShoppingCarts[F]) {
+    def createAndToCart(product: Product, cartId: String): Unit = {
+      for {
+        _ <- carts.create(cartId)
+        maybeSc <- carts.find(cartId)
+      } yield {
+        maybeSc match {
+          case Some(sc) => carts.add(sc, product)
+          case _ => maybeSc
+        }
+      }
+    }
+  }
+
 }
