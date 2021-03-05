b60dd5b743b43197748d8c0b3dc9fa8f7a8a1dce
Bartosz Mikulski <mail@mikulskibartosz.name>
2021-03-05 07:08:39 +0100

    correct indentations

--- play-scala/async-tasks/app/actors/ActorsModule.scala
+++ play-scala/async-tasks/app/actors/ActorsModule.scala
@@ -4,7 +4,7 @@ import com.google.inject.AbstractModule
 import play.libs.akka.AkkaGuiceSupport
 
 class ActorsModule extends AbstractModule with AkkaGuiceSupport {
-    override def configure(): Unit = {
-        bindActor(classOf[AsyncTaskInActor], "async-job-actor")
-    }
+  override def configure(): Unit = {
+    bindActor(classOf[AsyncTaskInActor], "async-job-actor")
+  }
 }
