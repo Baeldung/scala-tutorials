ccfdf85836211f3bf601113ee0ea50a47e4d361e
remis <remisharoon@gmail.com>
2021-03-19 23:57:56 +0400

    examples for @switch Annotation in Scala

---
+++ scala-core-4/src/main/scala/com/baeldung/scala/switchannotation/TableSwitchExample.scala
@@ -0,0 +1,16 @@
+package switchannotation
+import scala.annotation.switch
+
+object TableSwitchExample extends App{
+
+  val numWheels = 2
+
+  (numWheels : @switch) match {
+    case 1 => println("MonoWheeler")
+    case 2 => println("TwoWheeler")
+    case 3 => println("ThreeWheeler")
+    case 4 => println("FourWheeler")
+    case _ => println("UnKnown")
+  }
+
+}
