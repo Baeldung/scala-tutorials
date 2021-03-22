fa83aa02f782b207df4faa84513aa407ca577e03
remisharoon <remisharoon@users.noreply.github.com>
2021-03-22 09:43:57 +0400

    examples for @switch Annotation in Scala (#286)

---
+++ scala-core-4/src/main/scala/com/baeldung/scala/switchannotation/NotOptimizedExample.scala
@@ -0,0 +1,16 @@
+package switchannotation
+import scala.annotation.switch
+
+object NotOptimizedExample extends App{
+
+  val gender = 1
+  val male = 0
+  val female = 1
+
+  (gender : @switch) match {    // Compiler gives a warning: could not emit switch for @switch annotated match
+    case `male` => println("Male")
+    case `female` => println("Female")
+    case _ => println("Invalid")
+  }
+
+}
