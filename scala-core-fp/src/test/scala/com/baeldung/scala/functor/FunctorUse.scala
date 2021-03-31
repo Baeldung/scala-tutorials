8745ee3e28cb75aec4ef40c3d18545cabe73ab18
Andrey Filyanin <filyaniny@gmail.com>
2021-03-31 12:54:21 +0300

    AF [SCALA-160] - Change formatter options

--- scala-core-fp/src/test/scala/com/baeldung/scala/functor/FunctorUse.scala
+++ scala-core-fp/src/test/scala/com/baeldung/scala/functor/FunctorUse.scala
@@ -5,28 +5,28 @@ import cats.instances.option._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class FunctorSpec extends AnyFlatSpec {
-    "Option map" should "apply the function in an imperative way" in {
-        var patientIdOpt = Some("patient 0")
-        if (patientIdOpt.isDefined) {
-            val patientId = patientIdOpt.get
-            patientIdOpt = Some(s"$patientId is cured")
-        }
-
-        assert(patientIdOpt.contains("patient 0 is cured"))
+  "Option map" should "apply the function in an imperative way" in {
+    var patientIdOpt = Some("patient 0")
+    if (patientIdOpt.isDefined) {
+      val patientId = patientIdOpt.get
+      patientIdOpt = Some(s"$patientId is cured")
     }
 
-    "Option map" should "apply the function in an idiomatic way" in {
-        val temperatures = List(38, 39, 39, 37)
-        val panadol: Int => Int = temperature => temperature - 10
-        val lowTemperatures = temperatures.map(panadol)
+    assert(patientIdOpt.contains("patient 0 is cured"))
+  }
 
-        assert(lowTemperatures == List(28, 29, 29, 27))
-    }
+  "Option map" should "apply the function in an idiomatic way" in {
+    val temperatures = List(38, 39, 39, 37)
+    val panadol: Int => Int = temperature => temperature - 10
+    val lowTemperatures = temperatures.map(panadol)
 
-    "Option Functor map" should "apply the function to the internal value" in {
-        val option = Some("Hello!")
-        val result = Functor[Option].map(option)(_.toUpperCase)
+    assert(lowTemperatures == List(28, 29, 29, 27))
+  }
 
-        assert(result.contains("HELLO!"))
-    }
+  "Option Functor map" should "apply the function to the internal value" in {
+    val option = Some("Hello!")
+    val result = Functor[Option].map(option)(_.toUpperCase)
+
+    assert(result.contains("HELLO!"))
+  }
 }
