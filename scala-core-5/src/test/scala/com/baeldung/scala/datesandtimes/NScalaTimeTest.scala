d95fcb04868639a4726cde2bb81579c9ece5a2bd
remisharoon <remisharoon@users.noreply.github.com>
2021-06-15 09:07:07 +0400

    exmaples of SCALA-184 Working With Dates and Times in Scala (#320)

---
+++ scala-core-5/src/test/scala/com/baeldung/scala/datesandtimes/NScalaTimeTest.scala
@@ -0,0 +1,16 @@
+package com.baeldung.scala.datesandtimes
+import com.github.nscala_time.time.Imports._
+import org.joda.time.DateTime
+import org.scalatest.{FlatSpec, Matchers}
+
+class NScalaTimeTest extends FlatSpec with Matchers{
+  "measureElapsedTimePeriod" should "be able to calculate the time period between starting and ending DateTime objects and  return a Period object" in {
+
+    val processStart:DateTime = DateTime.now()
+    // process execution
+    val processEnd:DateTime = processStart + (1.hours + 10.minutes + 5.seconds)
+
+    val expectedElapsedTimePeriod:Period = Period.parse("PT1H10M5S")
+    NScalaTime.measureElapsedTimePeriod(processStart, processEnd) shouldEqual expectedElapsedTimePeriod
+  }
+}
