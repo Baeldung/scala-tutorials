40a6688023dcf8fa9386532db59e355c2eb27416
barbasa <ponch78@gmail.com>
2021-09-04 09:19:31 +0200

    SCALA-207: intersection types in Scala3 (#345)

---
+++ scala3-lang/src/main/scala/com/baeldung/scala3/intersectiontypes/BasicIntersectionType.scala
@@ -0,0 +1,54 @@
+package com.baeldung.scala3.intersectiontypes
+
+object BasicIntersectionType {
+
+  trait Scissors:
+    def cut: Unit
+
+  trait Niddle:
+    def sew: Unit
+
+  def fixDressOne(dressFixer: Scissors & Niddle) =
+    dressFixer.cut
+    dressFixer.sew
+
+  def fixDressTwo(dressFixer: Niddle & Scissors) =
+    dressFixer.cut
+    dressFixer.sew
+
+  object DressFixer extends Scissors, Niddle {
+    override def cut = print("Cutting dress ")
+
+    override def sew = print("Sewing dress ")
+  }
+
+  trait Knife:
+    def cut: Unit
+
+  trait Chainsaw:
+    def cut: Unit
+
+  object PaperCutter extends Knife, Scissors {
+    override def cut = print("Cutting stuff")
+  }
+
+  def cutPaper(pc: Knife & Scissors) =
+      pc.cut
+
+  trait OneGenerator:
+    def generate: Int = 1
+
+  trait TwoGenerator:
+    def generate: Int = 2
+
+  object NumberGenerator21 extends OneGenerator, TwoGenerator {
+    override def generate = super.generate
+  }
+
+  object NumberGenerator12 extends TwoGenerator, OneGenerator {
+    override def generate = super.generate
+  }
+
+  def generateNumbers(generator: OneGenerator & TwoGenerator) =
+      generator.generate
+}
---
+++ scala3-lang/src/main/scala/com/baeldung/scala3/intersectiontypes/DuckTyping.scala
@@ -0,0 +1,28 @@
+package com.baeldung.scala3.intersectiontypes
+import reflect.Selectable.reflectiveSelectable
+
+object DuckTyping {
+
+  class Scissors() {
+    def cut(): Unit = { println("Cutting with Scissors") }
+    val canCutPaper: Boolean = true
+  }
+
+  class Knife() {
+    def cut(): Unit = { println("Cutting with Knife") }
+    val canCutPaper: Boolean = true
+  }
+
+  class Chainsaw() {
+    def cut(): Unit = { println("Cutting with Chainsaw") }
+  }
+
+  type ToolToCutPaper = {
+    val canCutPaper: Boolean
+    def cut(): Unit
+  }
+
+  def cutPaper(pc: ToolToCutPaper) =
+    pc.cut()
+
+}
