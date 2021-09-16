befe3fde59be9e12a92db23eba64776f8cf2410e
Yadu Krishnan <yadavan88@gmail.com>
2021-09-16 09:34:40 +0200

    Added tests for extensions

---
+++ scala3-lang/src/main/scala/com/baeldung/scala3/implicits/extensions/StringExtensions.scala
@@ -0,0 +1,32 @@
+package com.baeldung.scala3.implicits.extensions
+
+object StringExtensions {
+  extension (str: String) {
+    def toSnakeCase = {
+      str.replaceAll("([A-Z])", "_" + "$1").toLowerCase
+    }
+
+    def isNumber = {
+      str.matches("[0-9]+")
+    }
+  }
+}
+
+object GenericExtensions {
+  extension[T] (list: List[T]) {
+    def getSecond = if (list.isEmpty) None else list.tail.headOption
+  }
+
+  extension[T: Numeric] (a: T) {
+    def add(b: T): T = {
+      val numeric = summon[Numeric[T]]
+      numeric.plus(a, b)
+    }
+  }
+
+  extension[T] (a: T)(using numeric: Numeric[T]) {
+    def add2(b: T): T = {
+      numeric.plus(a, b)
+    }
+  }
+}
