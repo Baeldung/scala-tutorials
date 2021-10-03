efda9341739238f24e77a33104a4b6caa0e88150
remis <remisharoon@gmail.com>
2021-10-03 22:20:33 +0400

    SCALA-200 Contextual Abstractions With Given Instances and Using Clauses examples

---
+++ scala3-lang/src/main/scala/com/baeldung/scala3/contextualabstractions/Givens.scala
@@ -0,0 +1,15 @@
+package com.baeldung.scala3.contextualabstractions
+
+object Givens extends App{
+
+  case class Item(name:String, price:Double)
+
+  given priceOrdering:Ordering[Item] = new Ordering[Item]{
+    override def compare(i1: Item, i2: Item): Int = i1.price.compareTo(i2.price)
+  }
+
+  given pageLimit:Int = 2
+
+  //Anonymous Givens
+  given Item = Item("Dummy", 0.0)
+}
