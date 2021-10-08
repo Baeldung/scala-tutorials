ec0bd0bec37932404aaee4fdd647ff1ee6af52c4
Yadu Krishnan <yadavan88@gmail.com>
2021-10-08 15:21:47 +0200

    Update InlineDef.scala

--- scala3-lang/src/main/scala/com/baeldung/scala3/macros/inline/InlineDef.scala
+++ scala3-lang/src/main/scala/com/baeldung/scala3/macros/inline/InlineDef.scala
@@ -1,5 +1,6 @@
 package com.baeldung.scala3.macros.inline
 object InlineDef {
   inline def answer(name: String): Unit = println(s"Elementary, my dear $name")
+  
   @main def defMain() = answer("Watson")
 }
