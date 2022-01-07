b0e82c65f21b92cc1807a8160ed9af403e3dbd6c
LucasSantiag <lucassantiago17.cardoso08@gmail.com>
2022-01-07 09:15:53 -0300

    fix test names

--- scala-core-collections/src/test/scala/com/baedung/scala/collections/ParallelCollectionTest.scala
+++ scala-core-collections/src/test/scala/com/baedung/scala/collections/ParallelCollectionTest.scala
@@ -4,11 +4,11 @@ import org.scalatest.FlatSpec
 
 class ParallelCollectionTest extends FlatSpec {
 
-  "Parallel list of 300" should "return half size" in {
+  "Parallel list of 100" should "return half size" in {
     assert((1 to 100).toList.par.count(_ % 2 == 0) == 50)
   }
 
-  "Parallel bla00" should "sum" in {
+  "Parallel list of 100" should "sum" in {
     assert((1 to 100).toList.par.sum == 5050)
   }
 }
