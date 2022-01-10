75a0e401bd9cb936475446cb235760f37d4075b5
LucasSantiag <lucassantiago17.cardoso08@gmail.com>
2022-01-10 10:05:45 -0300

    Fix tests

--- scala-core-collections/src/main/scala/com/baeldung/scala/collections/ParallelCollections.scala
+++ scala-core-collections/src/main/scala/com/baeldung/scala/collections/ParallelCollections.scala
@@ -3,15 +3,15 @@ package com.baeldung.scala.collections
 import scala.collection.parallel.immutable.{ParSeq, ParVector}
 
 object ParallelCollections {
-  val parallelList: ParSeq[Int] = (0 to 100).toList.par
+  lazy val parallelList: ParSeq[Int] = (0 to 100).toList.par
 
-  val parallelVector: ParVector[Int] = ParVector.range(0, 1000)
+  lazy val parallelVector: ParVector[Int] = ParVector.range(0, 1000)
 
-  val otherParallelVector: ParVector[Int] = ParVector.tabulate(1000)(x=>x)
+  lazy val otherParallelVector: ParVector[Int] = ParVector.tabulate(1000)(x=>x)
 
-  val parallelMap: ParSeq[Int] = parallelList.map(_*2)
+  lazy val parallelMap: ParSeq[Int] = parallelList.map(_*2)
 
-  val parallelFold: Int = parallelList.fold(0)(_ + _)
+  lazy val parallelFold: Int = parallelList.fold(0)(_ + _)
 
-  val parallelFilter: ParSeq[Int] = parallelList.filter(_ % 2 != 0)
+  lazy val parallelFilter: ParSeq[Int] = parallelList.filter(_ % 2 != 0)
 }
