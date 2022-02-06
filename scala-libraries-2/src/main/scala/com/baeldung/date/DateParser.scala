7048ca7b8430964ae510d6de8fe45ff390e7ee74
Bruno Unna <bunna@getvisibility.com>
2022-02-06 18:03:21 +0000

    Create the test for simple parsing

--- scala-libraries-2/src/main/scala/com/baeldung/date/DateParser.scala
+++ scala-libraries-2/src/main/scala/com/baeldung/date/DateParser.scala
@@ -1,3 +1,14 @@
 package com.baeldung.date
 
-class DateParser {}
+/**
+  * Simple case class to represent the elements from a date.
+  *
+  * @param year year of the date
+  * @param month month of the date (0-11)
+  * @param day day of the date (0-30)
+  */
+case class DateElements(year: Int, month: Int, day: Int)
+
+class DateParser {
+  def simpleParse(dateString: String): Option[DateElements] = ???
+}
