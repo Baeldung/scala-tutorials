fd12fdd5f107b2253dd38bf788f7f5a5c4b8b4e9
Yadukrishnan <yadukrishnan.krishnan@riskident.com>
2022-02-19 21:20:54 +0100

    added samples for pureconfig

---
+++ scala-libraries-2/src/main/scala/com/baeldung/pureconfig/Configs.scala
@@ -0,0 +1,74 @@
+package com.baeldung.pureconfig
+
+import enumeratum._
+import pureconfig._
+import pureconfig.configurable._
+import pureconfig.generic.auto._
+import pureconfig.generic.semiauto._
+
+import java.time.{LocalDate, LocalDateTime}
+import java.time.format.DateTimeFormatter
+import scala.concurrent.duration.FiniteDuration
+import pureconfig.module.enumeratum._
+
+sealed trait Protocol
+object Protocol {
+  implicit val seasonConvert: ConfigReader[Protocol] = deriveEnumerationReader[Protocol]
+  case object Http extends Protocol
+  case object Https extends Protocol
+}
+
+object impl {
+  implicit val localDateConvert = localDateConfigConvert(DateTimeFormatter.ISO_DATE)
+  implicit val localDateTimeConvert = localDateTimeConfigConvert(DateTimeFormatter.ISO_DATE_TIME)
+}
+
+final case class Port(number: Int) extends AnyVal
+
+final case class HttpConfig(
+  host: String,
+  port: Port,
+  protocol: Protocol,
+  defaultTimeout: FiniteDuration
+)
+
+final case class MonitoringServer(host: String, port: Port)
+
+final case class MonitoringConf(
+  enabled: Boolean,
+  servers: Seq[MonitoringServer]
+)
+
+/***  Config using Enumeratum based fields ***/
+
+sealed trait Env extends EnumEntry
+
+object Env extends Enum[Env] {
+  case object Prod extends Env
+  case object Test extends Env
+
+  override val values = findValues
+}
+
+final case class BaseAppConfig(name: String, startDate: LocalDate, env: Env)
+
+
+import pureconfig._
+import pureconfig.generic.auto._
+import pureconfig.module.enumeratum._
+import com.typesafe.config.ConfigFactory.parseString
+import enumeratum._
+import enumeratum.EnumEntry._
+
+sealed trait Greeting extends EnumEntry with Snakecase
+
+object Greeting extends Enum[Greeting] {
+  val values = findValues
+  case object Hello extends Greeting
+  case object GoodBye extends Greeting
+  case object ShoutGoodBye extends Greeting with Uppercase
+}
+
+case class GreetingConf(s: Greeting, e: Greeting)
+
+case class DatabaseConfig(url:String, databaseName:String)
---
+++ scala-libraries-2/src/test/scala/com/baeldung/pureconfig/SampleConfigLoader.scala
@@ -0,0 +1,70 @@
+package com.baeldung.pureconfig
+
+import com.typesafe.config.ConfigFactory.parseString
+import pureconfig.ConfigSource
+import pureconfig._
+import pureconfig.generic.auto._
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+import impl._
+import pureconfig.module.enumeratum._
+import pureconfig.generic.ProductHint
+import pureconfig.error.ConfigReaderException
+
+class SampleConfigLoader extends AnyWordSpec with Matchers {
+
+  "pureconfig" should {
+    "load config successfully" in {
+      val httpConf = ConfigSource.default.at("http").load[HttpConfig]
+      val monitoringConf =
+        ConfigSource.default.at("monitoring").load[MonitoringConf]
+
+      println(httpConf.isRight)
+      println(httpConf)
+      println(httpConf.right.get.defaultTimeout)
+
+      println(monitoringConf.isRight)
+      println(monitoringConf)
+
+      httpConf.isRight shouldBe true
+      monitoringConf.isRight shouldBe true
+
+      implicit def hint[A] =
+        ProductHint[A](ConfigFieldMapping(CamelCase, CamelCase))
+
+      val dummy = ConfigSource.default.loadOrThrow[BaseAppConfig]
+      println(dummy)
+
+    }
+
+    "load a config file other than application conf" in {
+      val dbConf = ConfigSource.resources("database.conf").load[DatabaseConfig]
+      dbConf.isRight shouldBe true
+      dbConf.right.get.databaseName shouldBe "configs"
+    }
+
+    "load a config from string content" in {
+      val strConf = ConfigSource
+        .string("""{"database-name": "strDB", "url":"mysql://localhost"}""")
+        .load[DatabaseConfig]
+      strConf.isRight shouldBe true
+      strConf.right.get.databaseName shouldBe "strDB"
+    }
+
+    "fail to load config if any field is not proper" in {
+      val strConf = ConfigSource
+        .string("""{"database-nam": "strDB", "url":"mysql://localhost"}""")
+        .load[DatabaseConfig]
+      strConf.isRight shouldBe false
+    }
+
+    "throw an exception if loadOrThrow is used" in {
+
+      assertThrows[ConfigReaderException[_]] {
+        ConfigSource
+          .string("""{"database-nam": "strDB", "url":"mysql://localhost"}""")
+          .loadOrThrow[DatabaseConfig]
+      }
+    }
+  }
+}
