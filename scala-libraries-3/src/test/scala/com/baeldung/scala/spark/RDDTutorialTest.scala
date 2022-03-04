59094d3b93b4c12f46f74b9a53e26a052d9c0687
LucasSantiag <lucassantiago17.cardoso08@gmail.com>
2022-03-04 07:42:04 -0300

    Fix identation

--- scala-libraries-3/src/test/scala/com/baeldung/scala/spark/RDDTutorialTest.scala
+++ scala-libraries-3/src/test/scala/com/baeldung/scala/spark/RDDTutorialTest.scala
@@ -7,34 +7,34 @@ import org.scalatest.flatspec.AnyFlatSpec
 class RDDTutorialTest extends AnyFlatSpec {
 
   "animals collection" should "became RDD size 4" in {
-    val animals = List("dog", "cat", "frog", "horse")
-    val animalsRDD: RDD[String] = sc.parallelize(animals)
-    assert(animalsRDD.count() == 4)
-    assert(animalsRDD.collect() sameElements Array("dog", "cat", "frog", "horse"))
+      val animals = List("dog", "cat", "frog", "horse")
+      val animalsRDD: RDD[String] = sc.parallelize(animals)
+      assert(animalsRDD.count() == 4)
+      assert(animalsRDD.collect() sameElements Array("dog", "cat", "frog", "horse"))
   }
 
   "animals rdd" should "filter animals without c" in {
-    val animals = List("dog", "cat", "frog", "horse")
-    val animalsRDD: RDD[String] = sc.parallelize(animals)
-    val noCRDD = animalsRDD.filter(_.startsWith("c"))
-    assert(noCRDD.collect() sameElements Array("cat"))
+      val animals = List("dog", "cat", "frog", "horse")
+      val animalsRDD: RDD[String] = sc.parallelize(animals)
+      val noCRDD = animalsRDD.filter(_.startsWith("c"))
+      assert(noCRDD.collect() sameElements Array("cat"))
   }
 
   "numbers sum" should "be equals to" in {
-    val numbers = sc.parallelize(List(1, 2, 3, 4, 5))
-    val numbersSum = numbers.reduce(_ + _)
-    assert(numbersSum == List(1, 2, 3, 4, 5).sum)
+      val numbers = sc.parallelize(List(1, 2, 3, 4, 5))
+      val numbersSum = numbers.reduce(_ + _)
+      assert(numbersSum == List(1, 2, 3, 4, 5).sum)
   }
 
   "joined rdd" should "contains both elements" in {
-    val rddOne = sc.parallelize(List((1, "cat"), (2, "dog"), (3, "frog")))
-    val rddTwo = sc.parallelize(List((1, "mammal"), (3, "amphibian")))
-    val joinedRDD = rddOne.join(rddTwo).collect
+      val rddOne = sc.parallelize(List((1, "cat"), (2, "dog"), (3, "frog")))
+      val rddTwo = sc.parallelize(List((1, "mammal"), (3, "amphibian")))
+      val joinedRDD = rddOne.join(rddTwo).collect
 
-    val firstElementJoined = ("cat", "mammal")
-    val secondElementJoined = ("frog", "amphibian")
+      val firstElementJoined = ("cat", "mammal")
+      val secondElementJoined = ("frog", "amphibian")
 
-    assert(joinedRDD.head._2 == firstElementJoined)
-    assert(joinedRDD.last._2 == secondElementJoined)
+      assert(joinedRDD.head._2 == firstElementJoined)
+      assert(joinedRDD.last._2 == secondElementJoined)
   }
 }
