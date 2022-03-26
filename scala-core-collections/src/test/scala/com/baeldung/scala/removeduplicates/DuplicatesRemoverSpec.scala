af859f2690c1a1e4e3cffbe5668ed1210e1a73c4
Bruno Unna <bunna@getvisibility.com>
2022-03-26 21:03:20 +0000

    Create simple test of list of ints

--- scala-core-collections/src/test/scala/com/baeldung/scala/removeduplicates/DuplicatesRemoverSpec.scala
+++ scala-core-collections/src/test/scala/com/baeldung/scala/removeduplicates/DuplicatesRemoverSpec.scala
@@ -5,6 +5,12 @@ import org.scalatest.wordspec.AnyWordSpec
 class DuplicatesRemoverSpec extends AnyWordSpec {
 
   "DuplicatesRemover" should {
-    "return a shorter list without duplicates" in {}
+    "return a shorter integers list without duplicates" in {
+      val withDuplicates = List(3, 7, 2, 7, 1, 3, 4)
+      val withoutDuplicates = List(3, 7, 2, 1, 4)
+      val deDuplicated =
+        DuplicatesRemover.removeDuplicates(withDuplicates)
+      assertResult(withoutDuplicates)(deDuplicated)
+    }
   }
 }
