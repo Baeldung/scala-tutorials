5a0e6e1d900811578d3c4e15785b70b76c564120
Bruno Unna <bunna@getvisibility.com>
2022-03-26 13:30:40 +0000

    Create skeleton for unit tests

---
+++ scala-core-collections/src/test/scala/com/baeldung/scala/removeduplicates/DuplicatesRemoverSpec.scala
@@ -0,0 +1,10 @@
+package com.baeldung.scala.removeduplicates
+
+import org.scalatest.wordspec.AnyWordSpec
+
+class DuplicatesRemoverSpec extends AnyWordSpec {
+
+  "DuplicatesRemover" should {
+    "return a shorter list without duplicates" in {}
+  }
+}
