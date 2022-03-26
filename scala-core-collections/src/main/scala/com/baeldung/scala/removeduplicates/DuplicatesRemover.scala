57dd496bc531f6d34b98402fa16d0800dbcbfc12
Bruno Unna <bunna@getvisibility.com>
2022-03-26 21:03:41 +0000

    Implement function to remove duplicates

--- scala-core-collections/src/main/scala/com/baeldung/scala/removeduplicates/DuplicatesRemover.scala
+++ scala-core-collections/src/main/scala/com/baeldung/scala/removeduplicates/DuplicatesRemover.scala
@@ -1,6 +1,15 @@
 package com.baeldung.scala.removeduplicates
 
 object DuplicatesRemover {
-  def removeDuplicatesNaively[T](list: List[Ordering[T]]): List[Ordering[T]] =
-    list.filterNot(_ => list.count(list.contains(_)) > 1)
+  private def assessElement[T: Ordering](init: List[T], last: T): List[T] = {
+    if (init.isEmpty) List(last)
+    else {
+      val recursiveList = assessElement(init.init, init.last)
+      if (init.contains(last)) recursiveList
+      else recursiveList :+ last
+    }
+  }
+
+  def removeDuplicates[T: Ordering](list: List[T]): List[T] =
+    assessElement(list.init, list.last)
 }
