dfdcb7fe321a1dae7f389f2f4271ff2616b5003f
Bruno Unna <bunna@getvisibility.com>
2022-03-27 12:25:09 +0100

    Include a unit test for objects

--- scala-core-collections/src/test/scala/com/baeldung/scala/removeduplicates/DuplicatesRemoverSpec.scala
+++ scala-core-collections/src/test/scala/com/baeldung/scala/removeduplicates/DuplicatesRemoverSpec.scala
@@ -2,6 +2,18 @@ package com.baeldung.scala.removeduplicates
 
 import org.scalatest.wordspec.AnyWordSpec
 
+import scala.language.implicitConversions
+
+case class Person(userId: String, firstName: String, lastName: String) {
+  override def hashCode(): Int = userId.hashCode
+
+  override def equals(other: Any): Boolean = {
+    if (!canEqual(other)) false
+    else if (!other.isInstanceOf[Person]) false
+    else userId == other.asInstanceOf[Person].userId
+  }
+}
+
 class DuplicatesRemoverSpec extends AnyWordSpec {
 
   "DuplicatesRemover" should {
@@ -22,6 +34,23 @@ class DuplicatesRemoverSpec extends AnyWordSpec {
       assertResult(List.empty[Int])(
         DuplicatesRemover.removeDuplicates(List.empty[Int])
       )
+    }
+    "de-duplicate lists of objects" in {
+      val withDuplicates = List(
+        Person(userId = "mm01", firstName = "Mickey", lastName = "Mouse"),
+        Person(userId = "jw04", firstName = "John", lastName = "Wayne"),
+        Person(userId = "mm01", firstName = "Marilyn", lastName = "Manson"),
+        Person(userId = "sh01", firstName = "Sherlock", lastName = "Holmes"),
+        Person(userId = "jw04", firstName = "John", lastName = "Watson")
+      )
+      val withoutDuplicates = List(
+        Person(userId = "mm01", firstName = "Mickey", lastName = "Mouse"),
+        Person(userId = "jw04", firstName = "John", lastName = "Wayne"),
+        Person(userId = "sh01", firstName = "Sherlock", lastName = "Holmes")
+      )
+      val deDuplicated =
+        DuplicatesRemover.removeDuplicates(withDuplicates)
+      assertResult(withoutDuplicates)(deDuplicated)
     }
   }
 }
