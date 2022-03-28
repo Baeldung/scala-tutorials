10009efe23a10f0c00a3c8cbdbd7d0292dcc5182
Bruno Unna <bunna@getvisibility.com>
2022-03-28 10:41:44 +0100

    Remove useless braces

--- scala-core-collections/src/main/scala/com/baeldung/scala/removeduplicates/DuplicatesRemover.scala
+++ scala-core-collections/src/main/scala/com/baeldung/scala/removeduplicates/DuplicatesRemover.scala
@@ -3,14 +3,13 @@ package com.baeldung.scala.removeduplicates
 object DuplicatesRemover {
 
   def removeDuplicatesRecursively[T](list: List[T]): List[T] = {
-    def assessRemoval(init: List[T], last: T): List[T] = {
+    def assessRemoval(init: List[T], last: T): List[T] =
       if (init.isEmpty) List(last)
       else {
         val recursiveList = assessRemoval(init.init, init.last)
         if (init.contains(last)) recursiveList
         else recursiveList :+ last
       }
-    }
 
     if (list.isEmpty) list
     else assessRemoval(list.init, list.last)
