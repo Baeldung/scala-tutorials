15342f4eac1497e2c8cc8611a78e4e0edaf823ce
vaibhav007jain <72961247+vaibhav007jain@users.noreply.github.com>
2022-04-19 08:37:12 +0530

    SCALA-242: Added code for Comparing Strings. (#423)
    * SCALA-242: Added code for Comparing Strings.
    
    * Added test class for comparing strings.
    
    * Added test class for comparing strings.
    
    * Updated test class for comparing strings.
    
    * Updated package statement.
    
    * Update StringComparisonUnitTest.scala
    
    Replaced AnyFeatureSpec with FeatureSpec
    
    * Update StringComparisonUnitTest.scala
    
    removed compiler errors

---
+++ scala-strings/src/test/scala/com/baeldung/scala/strings/comparison/StringComparisonUnitTest.scala
@@ -0,0 +1,67 @@
+package com.baeldung.scala.strings.comparison
+
+import org.scalatest.{FeatureSpec, GivenWhenThen}
+
+class StringComparisonUnitTest extends FeatureSpec with GivenWhenThen {
+
+  scenario("String Comparison with ==, eq and equals") {
+    
+    info("Demonstrate various scenarios on String comparison.")
+
+    Given("two same Strings")
+    val stringOne = "baeldung"
+    val stringTwo = "baeldung"
+
+    When("compared with == method")
+    val result1 = stringOne == stringTwo
+
+    And("compared with eq method")
+    val result2 = stringOne eq stringTwo
+
+    And("compared with equals method")
+    val result3 = stringOne.equals(stringTwo)
+
+    Then("the results will be true")
+    assert(result1 == true)
+    assert(result2 == true)
+    assert(result3 == true)
+  }
+
+  scenario("Handling of null String by == method") {
+
+    Given("two Strings out of which one String is null")
+    val stringOne = "baeldung"
+    val stringTwo = null
+
+    When("compared with == method")
+    val result1 = stringOne == stringTwo
+
+    Then("the result will be false")
+    assert(result1 == false)
+  }
+
+  scenario("Case-insensitive comparison") {
+
+    Given("two similar Strings which are case-insensitive")
+    val stringOne = "baeldung"
+    val stringTwo = "Baeldung"
+
+    When("compared with == method")
+    val result1 = stringOne == stringTwo
+
+    Then("the result will be false")
+    assert(result1 == false)
+
+    When("compared after converting toUpperCase")
+    val result2 = stringOne.toUpperCase() == stringTwo.toUpperCase()
+
+    Then("the result will be true")
+    assert(result2 == true)
+
+    When("compared with equalsIgnoreCase method")
+    val result3 = stringOne.equalsIgnoreCase(stringTwo)
+
+    Then("the result will be true")
+    assert(result3 == true)
+  }
+}
