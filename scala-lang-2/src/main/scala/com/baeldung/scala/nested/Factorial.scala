af51609a8efec7ddb027bc5f750cea5b049c3eb6
Yadu Krishnan <yadavan88@gmail.com>
2022-04-21 16:23:08 +0200

    Update Factorial.scala

--- scala-lang-2/src/main/scala/com/baeldung/scala/nested/Factorial.scala
+++ scala-lang-2/src/main/scala/com/baeldung/scala/nested/Factorial.scala
@@ -4,7 +4,7 @@ object Factorial {
   def factorial(num:Int): Int = {
     def recursiveFactorial(num: Int): Int = {
       num match {
-        case i if i <= 0 => throw new Exception("Enter number > 0")
+        case i if i <= 0 => throw new Exception("Enter a number greater than 0")
         case 1 => 1
         case n => n * recursiveFactorial(num-1)
       }
