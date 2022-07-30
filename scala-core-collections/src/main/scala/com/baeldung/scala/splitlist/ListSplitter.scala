9c39455af0f3fe54b4ce8f281aee4332d6b9a87e
Bruno Unna <bruno.unna@gmail.com>
2022-07-30 16:11:30 +0100

    Implement the splitter with a recursive function

--- scala-core-collections/src/main/scala/com/baeldung/scala/splitlist/ListSplitter.scala
+++ scala-core-collections/src/main/scala/com/baeldung/scala/splitlist/ListSplitter.scala
@@ -3,5 +3,24 @@ package com.baeldung.scala.splitlist
 import scala.util.Try
 
 object ListSplitter {
-  def split[A](list: List[A], i: Int): Try[List[List[A]]] = ???
+
+  @throws[IllegalArgumentException]("if the slice size is less than one")
+  private def recursiveSplit[A](list: List[A], n: Int): List[List[A]] =
+    if (n < 1)
+      throw new IllegalArgumentException("The minimum slice size is one")
+    else {
+      if (list.isEmpty) List.empty[List[A]]
+      else list.take(n) :: recursiveSplit(list.drop(n), n)
+    }
+
+  /** Dispatch method, to pick on of several implementations of the splitter.
+    *
+    * @param list original list to be split in sub-lists
+    * @param n desired size of the resulting sub-lists
+    * @tparam A type of the elements of the original list and its sub-lists
+    * @return a list of the sub-lists of `list`
+    */
+  def split[A](list: List[A], n: Int): Try[List[List[A]]] = {
+    Try(recursiveSplit(list, n))
+  }
 }
