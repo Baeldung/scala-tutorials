2fc7b436b22e37aa1939d8e3b6a31f4177dd211b
Bruno Unna <bruno.unna@gmail.com>
2022-07-30 16:45:38 +0100

    Implement the splitter using sliding()

--- scala-core-collections/src/main/scala/com/baeldung/scala/splitlist/ListSplitter.scala
+++ scala-core-collections/src/main/scala/com/baeldung/scala/splitlist/ListSplitter.scala
@@ -13,6 +13,12 @@ object ListSplitter {
       else list.take(n) :: recursiveSplit(list.drop(n), n)
     }
 
+  @throws[IllegalArgumentException]("if the slice size is less than one")
+  private def splitUsingSliding[A](list: List[A], n: Int): List[List[A]] =
+    if (n < 1)
+      throw new IllegalArgumentException("The minimum slice size is one")
+    else list.sliding(n, n).toList
+
   /** Dispatch method, to pick on of several implementations of the splitter.
     *
     * @param list original list to be split in sub-lists
@@ -20,7 +26,6 @@ object ListSplitter {
     * @tparam A type of the elements of the original list and its sub-lists
     * @return a list of the sub-lists of `list`
     */
-  def split[A](list: List[A], n: Int): Try[List[List[A]]] = {
-    Try(recursiveSplit(list, n))
-  }
+  def split[A](list: List[A], n: Int): Try[List[List[A]]] =
+    Try(splitUsingSliding(list, n))
 }
