4e829c031387be25e388750286d7163aabe28af2
Stefanos Georgakis <sgeorgakis.sg@gmail.com>
2022-08-23 12:32:53 +0300

    [SCALA-309] Remove method signatures to keep functions more readable

--- scala-akka-2/src/main/scala/com/baeldung/scala/akka_2/alpakka_sse/WikimediaSSEConsumer.scala
+++ scala-akka-2/src/main/scala/com/baeldung/scala/akka_2/alpakka_sse/WikimediaSSEConsumer.scala
@@ -1,31 +1,22 @@
 package com.baeldung.scala.akka_2.alpakka_sse
 
-import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.sse.ServerSentEvent
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse, Uri}
+import akka.http.scaladsl.model.{HttpRequest, Uri}
 import akka.stream.ThrottleMode
 import akka.stream.alpakka.sse.scaladsl.EventSource
-import akka.stream.scaladsl.{Keep, RunnableGraph, Sink, Source}
+import akka.stream.scaladsl.{Keep, Sink}
 
-import scala.collection.immutable
-import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
 
 class WikimediaSSEConsumer {
 
-  val wikiMediaUri: Uri = Uri(
-    "https://stream.wikimedia.org/v2/stream/recentchange"
-  )
+  val wikiMediaUri = Uri("https://stream.wikimedia.org/v2/stream/recentchange")
 
-  def sendRequest(request: HttpRequest)(implicit
-    actorSystem: ActorSystem
-  ): Future[HttpResponse] = Http().singleRequest(request)
+  def sendRequest(request: HttpRequest)(implicit actorSystem: ActorSystem) =
+    Http().singleRequest(request)
 
-  def eventSource(implicit
-    actorSystem: ActorSystem
-  ): Source[ServerSentEvent, NotUsed] =
+  def eventSource(implicit actorSystem: ActorSystem) =
     EventSource(
       uri = wikiMediaUri,
       send = sendRequest,
@@ -33,11 +24,7 @@ class WikimediaSSEConsumer {
       retryDelay = 1.second
     )
 
-  def eventsGraph(
-    nrOfSamples: Int
-  )(implicit
-    actorSystem: ActorSystem
-  ): RunnableGraph[Future[Seq[ServerSentEvent]]] =
+  def eventsGraph(nrOfSamples: Int)(implicit actorSystem: ActorSystem) =
     eventSource
       .throttle(
         elements = 1,
