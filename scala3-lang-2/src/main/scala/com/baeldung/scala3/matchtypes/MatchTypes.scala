cb45498f9e2b6bef87e9fb6ed56a5ea0cce7c6ff
Matteo Di Pirro <matteo.dipirro@kynetics.com>
2022-09-16 12:17:22 +0200

    SCALA-306 Added code for match types article

--- scala3-lang-2/src/main/scala/com/baeldung/scala3/matchtypes/MatchTypes.scala
+++ scala3-lang-2/src/main/scala/com/baeldung/scala3/matchtypes/MatchTypes.scala
@@ -1,13 +1,22 @@
 package com.baeldung.scala3.matchtypes
 
-object MatchTypes {
-  type FirstComponentOf[T] = T match {
-    case String => Option[Char]
-    case Int => Int
-    case Iterable[t] => Option[t]
-  }
+type FirstComponentOf[T] = T match
+  case String      => Option[Char]
+  case Int         => Int
+  case Iterable[t] => Option[t]
+  case Any         => T
 
-  val aNumber: FirstComponentOf[Int] = 2
-  val aChar: FirstComponentOf[String] = Some('b')
-  val anItem: FirstComponentOf[Seq[Float]] = Some(3.2f)
-}
+def firstComponentOf[U](elem: U): FirstComponentOf[U] = elem match
+  case s: String => if (s.nonEmpty) Some(s.charAt(0)) else Option.empty[Char]
+  case i: Int    => i.abs.toString.charAt(0).asDigit
+  case it: Iterable[_] => it.headOption
+  case a: Any          => a
+
+val aNumber: FirstComponentOf[Int] = 2
+val aChar: FirstComponentOf[String] = Some('b')
+val anItem: FirstComponentOf[Seq[Float]] = Some(3.2f)
+
+type Node[T] = T match
+  case Iterable[t] => Node[t]
+  case Array[t]    => Node[t]
+  case AnyVal      => T
