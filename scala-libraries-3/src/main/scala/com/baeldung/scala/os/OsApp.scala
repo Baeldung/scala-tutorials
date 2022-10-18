ed3c420f52bf7ce0d5171d0b795321c799bdebe6
Matteo Di Pirro <matteo.dipirro@kynetics.com>
2022-10-18 14:58:57 +0200

    SCALA-406 Add code for os-lib article

---
+++ scala-libraries-3/src/main/scala/com/baeldung/scala/os/OsApp.scala
@@ -0,0 +1,14 @@
+package com.baeldung.scala.os
+
+object OsApp extends App {
+  val ls = os.proc("ls", "-l").call()
+  println(ls.exitCode)
+  println(ls.out.text())
+  println(ls.err.text())
+
+  os.proc("ls", "-l", "dir").call()
+
+  os.proc("ls", "-l", "dir").call(check = false)
+
+  val lines = os.read(os.pwd / "LICENSE").lines.count()
+}
