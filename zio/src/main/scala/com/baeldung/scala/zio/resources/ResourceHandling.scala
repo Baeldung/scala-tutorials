d3e6f74775b0c89d74b6eb2dac837ce66126bd54
Yadukrishnan Krishnan <yadukrishnan@Yadukrishnans-MacBook-Air.local>
2022-11-05 15:42:58 +0100

    Added more example for resource handling

--- zio/src/main/scala/com/baeldung/scala/zio/resources/ResourceHandling.scala
+++ zio/src/main/scala/com/baeldung/scala/zio/resources/ResourceHandling.scala
@@ -23,12 +23,33 @@ object ResourceHandling extends ZIOAppDefault {
   }
 
 
-  def acquireFile = ZIO.succeed(println("acquiring file")) *> ZIO.succeed("Sauron.txt") *> ZIO.fail("ERROR")
+  def acquireFile = ZIO.succeed(println("acquiring file")) *> ZIO.succeed("Sauron.txt")
   def releaseFile(file: String) = ZIO.succeed(println("Closing file: "+file))
   val fileContentZIO = ZIO.acquireReleaseWith(acquireFile)(releaseFile) { file =>
     ZIO.succeed(println("Reading the content from the file: "+file)) *>
     ZIO.succeed("One ring to rule them all!")
   }
 
-  override def run = fileContentZIO 
+
+  def acquireDB = ZIO.succeed(println("OPening DB Connection")) *> ZIO.succeed("pgsql://localhost:5432")
+  def releaseDBCon(con: String) = ZIO.succeed(println("Closing DB Connection to URL: "+con))
+  val nestedResourceZIO = ZIO.acquireReleaseWith(acquireFile)(releaseFile) { file =>
+    ZIO.acquireReleaseWith(acquireDB)(releaseDBCon) { con =>
+      ZIO.succeed(println("Reading the content from the file: "+file + "Writing to DB: "+con)) *>
+      ZIO.succeed("One ring to rule them all AND bring them back!")
+    }
+  }
+
+  val acquiredResource = ZIO.acquireRelease(acquireFile)(releaseFile)
+  val acquireReleaseContent = for {
+    file <- acquiredResource
+    content <- ZIO.succeed(println("Reading from the acquired file: "+file)) *> ZIO.succeed("One ring to rule them all!")
+  } yield content
+  
+  def acquireFileWithFailure = ZIO.succeed(println("acquiring file")) *> ZIO.succeed("Sauron.txt") *> ZIO.fail("ERROR")
+  def fileContentFailedAcquire = ZIO.acquireReleaseWith(acquireFileWithFailure)(releaseFile) { file =>
+    ZIO.succeed(println("reading from file"))
+  }
+
+  override def run = nestedResourceZIO
 }
