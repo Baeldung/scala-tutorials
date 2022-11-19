35b084640a5f0cd03eb8b71221dbda22786522bb
Yadu Krishnan <yadavan88@gmail.com>
2022-11-19 14:26:52 +0100

    Formatted code

--- scala-libraries-4/src/test/scala/com/baeldung/scala/utest/NestedTest.scala
+++ scala-libraries-4/src/test/scala/com/baeldung/scala/utest/NestedTest.scala
@@ -3,22 +3,22 @@ package com.baeldung.scala.utest
 import utest._
 
 object NestedTest extends TestSuite {
-    override def tests: Tests = Tests {
-        test("outer test") - {
-            val list = List(1,2)
-            println("This is an outer level of the test.")
-            test("inner test 1") - {
-                val list2 = List(10,20)
-                list.zip(list2) ==> List((1,10), (2,20))
-            }
-            test("inner test 2") - {
-                val str = List("a", "b")
-                list.zip(str) ==> List((1,"a"), (2,"b"))
-            }
-        }
-        test("outer test 2") - {
-            println("there is no nesting level here") 
-            assert(true)
-        }
+  override def tests: Tests = Tests {
+    test("outer test") - {
+      val list = List(1, 2)
+      println("This is an outer level of the test.")
+      test("inner test 1") - {
+        val list2 = List(10, 20)
+        list.zip(list2) ==> List((1, 10), (2, 20))
+      }
+      test("inner test 2") - {
+        val str = List("a", "b")
+        list.zip(str) ==> List((1, "a"), (2, "b"))
+      }
     }
+    test("outer test 2") - {
+      println("there is no nesting level here")
+      assert(true)
+    }
+  }
 }
--- scala-libraries-4/src/test/scala/com/baeldung/scala/utest/RetryTest.scala
+++ scala-libraries-4/src/test/scala/com/baeldung/scala/utest/RetryTest.scala
@@ -6,7 +6,8 @@ import scala.util.Random
 
 object RetryTest extends TestSuite {
   override def tests: Tests = Tests {
-    def flakyMethod: Int = Random.nextInt(2) //change this value to test failure
+    def flakyMethod: Int =
+      Random.nextInt(2) // change this value to test failure
     test("retry flaky test") - retry(3) {
       val value = flakyMethod
       assert(value < 2)
