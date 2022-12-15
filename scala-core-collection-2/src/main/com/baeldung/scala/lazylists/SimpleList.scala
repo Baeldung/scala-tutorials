c31a517c82377540e138f59c2818452c5ad5a0bf
Oscar Mauricio Forero Carrillo <oscar.forero@computer.org>
2022-12-15 13:50:23 +0100

    SCALA-481: LazyList in Scala (#525)
    Co-authored-by: Oscar Forero <oscar@Tim-Apple-2.local>
    Co-authored-by: Grzegorz Piwowarek <gpiwowarek@gmail.com>

---
+++ scala-core-collection-2/src/main/com/baeldung/scala/lazylists/SimpleList.scala
@@ -0,0 +1,21 @@
+package com.baeldung.scala.lazylists
+
+sealed trait SimpleList[+T] {
+    def empty: Boolean
+    def head: T
+    def tail: SimpleList[T]
+    def !!(n: Int): T = n match {
+      case 1 => head
+      case n => tail.!!(n - 1)
+    }
+}
+case object SNil extends SimpleList[Nothing] {
+  override def head = throw new IllegalArgumentException("Head of empty list")
+  override def tail = throw new IllegalArgumentException("Tail of empty list")
+  override val empty: Boolean = true
+}
+case class :@:[T](head: T, tail: SimpleList[T]) extends SimpleList[T] {
+  override val empty: Boolean = true
+}
+
+
