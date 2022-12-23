ca76461469dac8e755157b9586808f9eea3ef1ee
Bruno Unna <bruno.unna@gmail.com>
2022-12-23 10:49:51 +0000

    Add a few test cases

--- scala-strings/src/test/scala/com/baeldung/scala/strings/camelcase/CamelCaseSpec.scala
+++ scala-strings/src/test/scala/com/baeldung/scala/strings/camelcase/CamelCaseSpec.scala
@@ -1,8 +1,8 @@
 package com.baeldung.scala.strings.camelcase
 
-import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.WordSpec
 
-class CamelCaseSpec extends AnyWordSpec {
+class CamelCaseSpec extends WordSpec {
 
   import StringWrapper._
 
@@ -10,6 +10,19 @@ class CamelCaseSpec extends AnyWordSpec {
     "do nothing to strings already in camelCase" in {
       val camelCase = "thisStringIsAlreadyInCamelCase"
       assertResult(camelCase)(camelCase.toCamelCase)
+    }
+    "handle the empty string gracefully" in {
+      assertResult("")("".toCamelCase)
+    }
+    "remove spaces and change case" in {
+      val notCamelCase = "A string With DivErSe CASEs"
+      val camelCase = "aStringWithDiverseCases"
+      assertResult(camelCase)(notCamelCase.toCamelCase)
+    }
+    "remove underscores and spaces" in {
+      val notCamelCase = "I don't like_snakes_becauseThey_BITE"
+      val camelCase = "iDon'tLikeSnakesBecauseTheyBite"
+      assertResult(camelCase)(notCamelCase.toCamelCase)
     }
   }
 
