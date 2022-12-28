d15ed71ce706c0f62bf50851d18d6cdda1d38057
Bruno Unna <bruno.unna@gmail.com>
2022-12-28 22:49:15 +0000

    Implement a recursive scan approach

--- scala-core-collections-2/src/main/scala/com/baeldung/scala/lastelement/Finder.scala
+++ scala-core-collections-2/src/main/scala/com/baeldung/scala/lastelement/Finder.scala
@@ -1,7 +1,29 @@
 package com.baeldung.scala.lastelement
 
+import scala.annotation.tailrec
+
 object Finder {
   implicit class Wrapper[T](val list: List[T]) extends AnyVal {
-    def lastWhen(predicate: T => Boolean): Int = 0
+
+    /** Extension method that gives List instances the possibility to find their
+      * last element that satisfies a given predicate.
+      *
+      * @param predicate
+      *   the condition for which the last satisfying element will be searched
+      *   for
+      * @return
+      *   index (zero-based) of the last element that satisfies the condition,
+      *   or a negative number if it's not found
+      */
+    def lastWhen(predicate: T => Boolean): Int =
+      useRecursiveScan(list, predicate)
+
+    @tailrec
+    private def useRecursiveScan(list: List[T], predicate: T => Boolean): Int =
+      if (list.isEmpty) -1
+      else if (predicate(list.last)) list.size - 1
+      else useRecursiveScan(list.take(list.size - 1), predicate)
+
   }
+
 }
