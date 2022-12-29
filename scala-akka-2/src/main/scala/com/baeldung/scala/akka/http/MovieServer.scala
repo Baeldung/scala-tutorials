7f8941f28d89fee7f6e6171cd95c41276ff954be
Yadu Krishnan <yadavan88@gmail.com>
2022-12-29 22:44:02 +0100

    Akka HTTP Examples

--- scala-akka-2/src/main/scala/com/baeldung/scala/akka/http/MovieServer.scala
+++ scala-akka-2/src/main/scala/com/baeldung/scala/akka/http/MovieServer.scala
@@ -8,37 +8,69 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
-import scala.util.Success
+import scala.util._
+import akka.http.scaladsl.model.headers.RawHeader
 
 object MovieServer extends App {
 
-    implicit val system = ActorSystem("MoviesServer")
-    val movieService = new MovieService()
-    implicit val movieFormat = jsonFormat2(Movie)
+  implicit val system = ActorSystem("MoviesServer")
+  val movieService = new MovieService()
+  implicit val movieFormat = jsonFormat3(Movie)
 
-    val route = path("movies" / "heartbeat") {
-        get {
-            complete("Success")
-        }
-    } ~ path("movies") {
-        get {            
-            onComplete(movieService.getAllMovies()){
-                case util.Success(res) => complete(res)
-                case util.Failure(ex) => complete(StatusCodes.InternalServerError)
-            }
-        }
-    } ~ path("movies" / Segment / IntNumber) { (name, length) =>
-        get {            
-            onComplete(movieService.saveMovie(Movie(name,length))){
-                case util.Success(res) => complete("Saved")
-                case util.Failure(ex) => complete(StatusCodes.InternalServerError)
-            }
-        }
+  val route = path("movies" / "heartbeat") {
+    get {
+      complete("Success")
     }
-
-
-    val server = Http().newServerAt("localhost", 9090).bind(route)
-    server.foreach{ s=>
-        println("Successfully started on localhost:9090 ")
+  } ~ path("movies" / "test") {
+    get {
+      complete("Verified")
     }
+  } ~ path("movies-sync") {
+    complete(movieService.getAllMoviesSync())
+  } ~ path("movies") {
+    get {
+      onComplete(movieService.getAllMovies()) {
+        case util.Success(res) => complete(res)
+        case util.Failure(ex)  => complete(StatusCodes.InternalServerError)
+      }
+    } ~ post {
+      entity(as[Movie]) { movie =>
+        onComplete(movieService.saveMovie(movie)) {
+          case Success(res) => complete(res)
+          case Failure(ex)  => complete(StatusCodes.InternalServerError)
+        }
+      }
+    }
+  } ~ path("movies" / IntNumber) { id =>
+    put {
+      entity(as[Movie]) { movie =>
+        onComplete(movieService.updateMovie(id, movie)) {
+          case Success(res) => complete(res)
+          case Failure(ex)  => complete(StatusCodes.InternalServerError)
+        }
+      }
+    }
+  } ~ path("movies" / IntNumber) { id =>
+    delete {
+      onComplete(movieService.deleteMovie(id)) {
+        case Success(res) => complete(res)
+        case Failure(ex)  => complete(StatusCodes.InternalServerError)
+      }
+    }
+  } ~ path("test-headers") {
+    get {
+      optionalHeaderValueByName("apiKey") { apiKey =>
+        respondWithHeader(RawHeader("authenticated", "done")) {
+          complete(s"Received apiKey in header as '$apiKey' ")
+        }
+      }
+    }
+  }
+
+  val server = Http().newServerAt("localhost", 9090).bind(route)
+  server.map { _ =>
+    println("Successfully started on localhost:9090 ")
+  } recover { case ex =>
+    println("Failed to start the server due to: " + ex.getMessage)
+  }
 }
