c1770036fafda08063192c9b32e3031981337eb4
Bruno Unna <bruno.unna@gmail.com>
2023-01-14 23:38:34 +0000

    Simplify the Politician trait

--- scala-lang-2/src/main/scala/com/baeldung/scala/withtrait/Politician.scala
+++ scala-lang-2/src/main/scala/com/baeldung/scala/withtrait/Politician.scala
@@ -1,11 +1,9 @@
 package com.baeldung.scala.withtrait
 
 trait Politician {
-  val politicalParty, politicalOrientation, charge: String
-
   def vote(): String =
     "I'm voting"
 
-  def saySpeech(): String =
-    "I'm saying an important speech: blah, blah, blah..."
+  def saySpeech(speech: String): String =
+    s"I'm saying an important speech: $speech"
 }
