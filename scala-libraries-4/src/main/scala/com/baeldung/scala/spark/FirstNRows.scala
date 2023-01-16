3a4b36c8bda9bde3bbe4b33d91b92ae85e863bec
Kateu Herbert <hkateu@gmail.com>
2023-01-16 19:16:02 +0300

    First n rows of a Dataframe (#540)
    * Abstract classes vs traits article
    
    * Update README.md
    
    * Revert "Abstract classes vs traits article"
    
    This reverts commit ba5c7da25bdea71a6cc7fdf5d0b78d102a0b07aa.
    
    * Took off the link from readme
    
    * added FirstNRows code
    
    * Edited println and added a show method
    
    * Added link in readme

---
+++ scala-libraries-4/src/main/scala/com/baeldung/scala/spark/FirstNRows.scala
@@ -0,0 +1,66 @@
+package com.baeldung.scala.spark
+import sparkUtil.getSpark
+
+object info {
+  val spark = getSpark("FirstNRows")
+  import spark.implicits._
+
+  val data = Seq(
+    ("Ann", 25),
+    ("Brian", 16),
+    ("Jack", 35),
+    ("Conrad", 27),
+    ("Grace", 33),
+    ("Richard", 40)
+  ).toDF("Name", "Age")
+}
+
+object FirstNRows extends App {
+  import info.data
+  import info.spark
+
+  data.show(3)
+
+  /** | Name  | Age |
+    * |:------|:----|
+    * | Ann   | 25  |
+    * | Brian | 16  |
+    * | Jack  | 35  |
+    * only showing top 3 rows
+    */
+
+  data.head(2).foreach(println)
+
+  /** [Ann,25] [Brian,16]
+    */
+
+  data.take(2).foreach(println)
+
+  /** [Ann,25] [Brian,16]
+    */
+
+  println(data.takeAsList(2))
+
+  /** [[Ann,25], [Brian,16]]
+    */
+
+  data.limit(2).foreach(println(_))
+
+  /** [Ann,25] [Brian,16]
+    */
+
+  data.limit(2).show()
+
+  /** | Name  | Age |
+    * |:------|:----|
+    * | Ann   | 25  |
+    * | Brian | 16  |
+    */    
+
+  println(data.first())
+
+  /** [Ann,25]
+    */
+
+  spark.close()
+}
---
+++ scala-libraries-4/src/main/scala/com/baeldung/scala/spark/firstNRows/sparkUtility.scala
@@ -0,0 +1,11 @@
+package com.baeldung.scala.spark
+import org.apache.spark.sql.SparkSession
+
+object sparkUtil {
+  def getSpark(Name: String, Master: String = "local", partNo: String = "5", verbose:Boolean = true): SparkSession = {
+    val sparkSession = SparkSession.builder().appName(Name).master(Master).getOrCreate()
+    sparkSession.conf.set("spark.sql.shuffle.partitions", partNo)
+    if(verbose) println(s"Session started on spark version ${sparkSession.version}")
+    return sparkSession
+  }
+}
