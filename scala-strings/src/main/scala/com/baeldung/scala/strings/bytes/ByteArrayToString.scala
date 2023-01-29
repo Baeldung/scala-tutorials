6f9a1d8a8359acb6b209f471284f3f34a7b7eee3
Callum Gibbons <callumgibbons@Callums-MacBook-Pro.local>
2023-01-29 13:38:03 +0000

    [SCALA-508] Convert Byte Array to String in Scala

---
+++ scala-strings/src/main/scala/com/baeldung/scala/strings/bytes/ByteArrayToString.scala
@@ -0,0 +1,32 @@
+package com.baeldung.scala.strings.bytes
+
+import java.nio.charset.StandardCharsets
+
+object ByteArrayToString {
+  def main(args: Array[String]): Unit = {
+    val helloInUtf16 = Array[Byte](104, 101, 108, 108, 111)
+    val helloInUtf16Le = Array[Byte](104, 0, 101, 0, 108, 0, 108, 0, 111, 0)
+
+    usingToString(helloInUtf16)
+    usingNewString(helloInUtf16)
+    usingToChar(helloInUtf16)
+    usingDifferentCharSet(helloInUtf16Le)
+  }
+
+  def usingToString(bytes: Array[Byte]) = {
+    bytes.toString()
+  }
+
+  def usingNewString(bytes: Array[Byte]) = {
+    new String(bytes, StandardCharsets.UTF_16)
+  }
+
+  def usingToChar(bytes: Array[Byte]) = {
+    bytes.map(_.toChar).mkString
+  }
+
+  def usingDifferentCharSet(bytes: Array[Byte]) = {
+    new String(bytes, StandardCharsets.UTF_16LE)
+  }
+
+}
