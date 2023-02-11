cbd5402a963c6292c73df7d0b14bac187be6b7da
Yadu Krishnan <yadavan88@gmail.com>
2023-02-11 15:02:31 +0100

    Added more tests

--- scala-libraries-4/src/test/scala/com/baeldung/scala/cats/writer/WriterMonadUnitTest.scala
+++ scala-libraries-4/src/test/scala/com/baeldung/scala/cats/writer/WriterMonadUnitTest.scala
@@ -1,15 +1,31 @@
 package com.baeldung.scala.cats.writer
 
-import cats.data.Writer
+import cats.Id
+import cats.data.{Writer, WriterT}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-final case class User(id:Long, name: String)
-final case class Account(name:String, address: String, country: String)
+final case class User(id: Long, name: String)
+final case class Account(name: String, address: String, country: String)
 class WriterMonadUnitTest extends AnyFlatSpec with Matchers {
 
-  def getUser(token: String): User = User(1,"user-1")
+  def getUser(token: String): User = User(1, "user-1")
   def getAccount(user: User): Account = Account("User 1", "Str 1", "Germany")
+
+  it should "create a simple writer monad" in {
+    val writer: Writer[String, Int] = Writer("Multiplication", 5 * 5)
+    val (desc, value) = writer.run
+    value shouldBe 25
+    desc shouldBe "Multiplication"
+  }
+
+  it should "apply for-comprehension to multiple writer" in {
+    val combined: Writer[String, Int] = for {
+      writer1 <- Writer("Init Value,", 10)
+      writer2 <- Writer("Multiplication", 5)
+    } yield writer1 * writer2
+    combined.run shouldBe ("Init Value,Multiplication", 50)
+  }
 
   it should "create writer monad and execute it" in {
     val writer = Writer("Get User from token", getUser("token"))
@@ -46,13 +62,38 @@ class WriterMonadUnitTest extends AnyFlatSpec with Matchers {
       account <- Writer(",Getting account info", getAccount(user))
       _ <- Writer.tell(",completed")
     } yield account
-
-
     val (logs, acc) = combinedWriter.run
-
     logs shouldBe "Get User from token,Getting account info,completed"
     acc.name shouldBe "User 1"
+  }
 
+  it should "modify the value side using map function" in {
+    val writer = Writer("Get User from token", getUser("token"))
+    val capitalizedWriter = writer.map(_.name.toUpperCase)
+    val (log, name) = capitalizedWriter.run
+    name shouldBe "USER-1"
+    log shouldBe "Get User from token"
+  }
+
+  it should "clear the log side of writer" in {
+    val writer = Writer("Get User from token", getUser("token"))
+    val resetWriter = writer.reset
+    val (log, usr) = resetWriter.run
+    usr.name shouldBe "user-1"
+    log shouldBe empty
+  }
+
+  it should "get the value from writer" in {
+    val writer = Writer("Get User from token", getUser("token"))
+    val usr = writer.value
+    usr.name shouldBe "user-1"
+  }
+
+  it should "extract the value as a tuple" in {
+    val writer = Writer("Get User from token", getUser("token"))
+    val (log, tuple) = writer.listen.run
+    log shouldBe tuple._2
+    tuple._1.name shouldBe "user-1"
   }
 
 }
