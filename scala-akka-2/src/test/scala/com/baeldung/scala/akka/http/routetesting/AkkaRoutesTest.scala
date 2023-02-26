9731d3c53b95ba295b7c90095261f3d6d805a5a2
Callum Gibbons <callumgibbons@Callums-MacBook-Pro.local>
2023-02-26 17:00:58 +0000

    [SCALA-559] Updated unit tests

--- scala-akka-2/src/test/scala/com/baeldung/scala/akka/http/routetesting/AkkaRoutesTest.scala
+++ scala-akka-2/src/test/scala/com/baeldung/scala/akka/http/routetesting/AkkaRoutesTest.scala
@@ -1,36 +1,46 @@
 package com.baeldung.scala.akka.http.routetesting
 
 import akka.http.scaladsl.model.headers.RawHeader
-import akka.http.scaladsl.model.{HttpMethods, StatusCodes}
+import akka.http.scaladsl.model.{ContentTypes, HttpMethods, StatusCodes}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import com.baeldung.scala.akka.http.routetesting.AkkaRoutes.routes
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class AkkaRoutesTest extends AnyFlatSpec with Matchers with ScalatestRouteTest {
-  "a" should "b" in {
+  "/hello" should "return Hello World!" in {
     Get("/hello") ~> routes ~> check {
       status shouldEqual StatusCodes.OK
       responseAs[String] shouldBe "Hello World!"
     }
   }
-  "a" should "c" in {
-    Get("/hello-name?name=John") ~> routes ~> check {
+  "/hello-name" should "return hello to the name provided" in {
+    val testName = "John"
+    Get(s"/hello-name?name=$testName") ~> routes ~> check {
       status shouldEqual StatusCodes.OK
-      responseAs[String] shouldBe "Hello John"
+      responseAs[String] shouldBe s"Hello $testName"
+      headers.length shouldBe 0
+      contentType shouldBe ContentTypes.`text/plain(UTF-8)`
     }
   }
-  "a" should "d" in {
+  "/hello-user" should "return 200 for X-User-Id of 123" in {
     Get("/hello-user") ~> RawHeader("X-User-Id", "123") ~> routes ~> check {
       status shouldEqual StatusCodes.OK
     }
-    Get("/hello-user") ~> addHeader("X-User-Id", "123") ~> routes ~> check {
-      status shouldEqual StatusCodes.OK
+  }
+
+  "/hello-user" should "return 401 for X-User-Id of 456" in {
+    Get("/hello-user") ~> addHeader("X-User-Id", "456") ~> routes ~> check {
+      status shouldEqual StatusCodes.Unauthorized
+      responseAs[String] shouldBe "Incorrect user"
     }
   }
-  "a" should "e" in {
-    new RequestBuilder(HttpMethods.POST)("/hello-name", "Bob") ~> routes ~> check {
-      responseAs[String] shouldBe "Hello Bob"
+
+  "hello-name" should "return hello to name provided in request body" in {
+    val testName = "Sarah"
+    new RequestBuilder(HttpMethods.POST)("/hello-name", testName) ~> routes ~> check {
+      status shouldEqual StatusCodes.OK
+      responseAs[String] shouldBe s"Hello $testName"
     }
   }
 }
