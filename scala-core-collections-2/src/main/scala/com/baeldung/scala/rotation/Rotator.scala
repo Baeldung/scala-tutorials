4b51d164e2bfe133a485ab810640cdee5271e2c7
Bruno Unna <bruno.unna@gmail.com>
2023-03-04 13:31:06 +0000

    Implement the rotatedView using splitAt()

--- scala-core-collections-2/src/main/scala/com/baeldung/scala/rotation/Rotator.scala
+++ scala-core-collections-2/src/main/scala/com/baeldung/scala/rotation/Rotator.scala
@@ -5,7 +5,15 @@ object Rotator {
 
     def rotatedView(index: Int): Seq[T] = useSplit(sequence, index)
 
-    private def useSplit(sequence: Seq[T], index: Int): Seq[T] = ???
+    private def useSplit(sequence: Seq[T], index: Int): Seq[T] = {
+      val length = sequence.length
+      if (length == 0) sequence
+      else {
+        val normalisedIndex = (index % length + length) % length
+        val (left, right) = sequence.splitAt(normalisedIndex).swap
+        left ++ right
+      }
+    }
 
   }
 
