64948e236dc37f5ca1f39a727b56a84a66d0574b
Yadu Krishnan <yadavan88@gmail.com>
2023-03-20 21:13:46 +0100

    Changed logic in timer test to avoid flakiness, also added retry

--- scala-akka/src/test/scala-2/com/baeldung/scala/akka/scheduler/TimerUnitTest.scala
+++ scala-akka/src/test/scala-2/com/baeldung/scala/akka/scheduler/TimerUnitTest.scala
@@ -2,26 +2,39 @@ package com.baeldung.scala.akka.scheduler
 
 import akka.actor.{ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
-import org.scalatest.Ignore
+import org.scalatest.{Ignore, Retries}
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.tags.Retryable
 import org.scalatest.wordspec.AnyWordSpecLike
 
 import scala.concurrent.duration._
 
-@Ignore //fixing in JAVA-9843
+@Retryable
 class TimerUnitTest
   extends TestKit(ActorSystem("test-system"))
   with ImplicitSender
   with AnyWordSpecLike
-  with Matchers {
+  with Matchers
+  with Retries {
+
+  override def withFixture(test: NoArgTest) = {
+    if (isRetryable(test))
+      withRetry {
+        super.withFixture(test)
+      }
+    else
+      super.withFixture(test)
+  }
 
   "Timer Actor" must {
     "reply with a msg after timer ticks periodically" in {
       val timerActor = system.actorOf(Props(classOf[TimerActor], self))
-      expectMsg(350.millis, "Periodic-Tick")
-      expectMsg(350.millis, "Periodic-Tick")
-      expectMsg(350.millis, "Periodic-Tick")
+      val msgs = receiveN(3, 700.millis)
       system.stop(timerActor)
+      assert(
+        msgs.size >= 3
+      ) // just in case the 4th msg is produced by the timer before stopping
+      assert(msgs.toSet == Set("Periodic-Tick"))
     }
   }
 
