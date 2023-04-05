3195a65fb5ed6dc474819de299279c4683f3d0c1
Kateu Herbert <hkateu@gmail.com>
2023-04-05 18:39:02 +0300

    added comment

--- scala-libraries-4/src/main/scala-2/com/baeldung/scala/sparkJoins/sparkJoin.scala
+++ scala-libraries-4/src/main/scala-2/com/baeldung/scala/sparkJoins/sparkJoin.scala
@@ -37,7 +37,7 @@ object SparkJoins {
   val innerJoin =
     TopHorrorsIGN2022.join(TopHorrorsTheAVClub2022, Seq("IMDB Rating"))
 
-  // produces same results as innerJoinWithQuery
+  // inner join with explicit join type
   val innerJoin_v2 =
     TopHorrorsIGN2022.join(TopHorrorsTheAVClub2022, Seq("IMDB Rating"), "inner")
 
