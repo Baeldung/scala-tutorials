30cb725b6a1501e34bbe476d323f387c9bc5e3b8
Bruno Unna <bruno.unna@gmail.com>
2023-04-13 17:52:41 +0100

    Let sbtfmt do the formatting, because I can't

--- scala-core-8/src/main/scala-2/com/baeldung/scala/builderpattern/SafeGuitarBuilder.scala
+++ scala-core-8/src/main/scala-2/com/baeldung/scala/builderpattern/SafeGuitarBuilder.scala
@@ -12,10 +12,10 @@ case class SafeGuitarBuilder[Electric <: TBoolean] private (
   reverb: Float = 0.0f,
   delay: Int = 0
 ) {
-  def electric: SafeGuitarBuilder[TTrue] =
-    copy[TTrue](isElectric = true)
-
-  def withReverb(reverb: Float)(implicit ev: Electric =:= TTrue): SafeGuitarBuilder[TTrue] =
+  def electric: SafeGuitarBuilder[TTrue] = copy[TTrue](isElectric = true)
+  def withReverb(reverb: Float)(implicit
+    ev: Electric =:= TTrue
+  ): SafeGuitarBuilder[TTrue] =
     copy[TTrue](reverb = reverb)
 
   def build(): Guitar = Guitar(
