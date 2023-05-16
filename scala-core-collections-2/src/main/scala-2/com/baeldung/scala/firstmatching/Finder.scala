766c614356165dd7152e3992a61aa068946545ca
Bruno Unna <bruno.unna@gmail.com>
2023-05-16 20:40:30 +0100

    Implement the recursive findMatch() method

--- scala-core-collections-2/src/main/scala-2/com/baeldung/scala/firstmatching/Finder.scala
+++ scala-core-collections-2/src/main/scala-2/com/baeldung/scala/firstmatching/Finder.scala
@@ -1,9 +1,14 @@
 package com.baeldung.scala.firstmatching
 
+import scala.annotation.tailrec
+
 object Finder {
   implicit class Wrapper[T](val sequence: Seq[T]) extends AnyVal {
-    def findMatch(condition: T => Boolean): Option[T] = {
-      sequence.find(condition)
+    @tailrec
+    final def findMatch(condition: T => Boolean): Option[T] = {
+      if (sequence.isEmpty) None
+      else if (condition(sequence.head)) Some(sequence.head)
+      else sequence.tail.findMatch(condition)
     }
   }
 }
