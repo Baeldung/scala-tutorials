d39135a364c20f848a48e1ebb4f94eede9289f1d
Yadu Krishnan <yadavan88@gmail.com>
2023-06-01 08:41:20 +0200

    Added more tests

--- scala3-lang-2/src/test/scala/com/baeldung/scala3/nonfatal/NonFatalExceptionsTest.scala
+++ scala3-lang-2/src/test/scala/com/baeldung/scala3/nonfatal/NonFatalExceptionsTest.scala
@@ -2,12 +2,57 @@ package com.baeldung.scala3.nonfatal
 
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+
 import scala.util.*
-import scala.util.control.NonFatal
+import scala.util.control.{ControlThrowable, NonFatal}
 
 class NonFatalExceptionsTest extends AnyWordSpec with Matchers {
 
   "NonFatalExceptionsTest" should {
+
+    "handle different exceptions in catch block" in {
+      def failingFn(num: Int): String = {
+        num match {
+          case 0 => throw new RuntimeException("Number 0 is not allowed")
+          case n if n < 0 =>
+            throw new IllegalStateException("Number can't be negative")
+          case _ => "Success"
+        }
+      }
+      val handledResult =
+        try {
+          failingFn(-3)
+        } catch {
+          case is: IllegalStateException => "IllegalStateException"
+          case rt: RuntimeException      => "RuntimeException"
+          case _                         => "Unknown"
+        }
+      handledResult shouldBe "IllegalStateException"
+    }
+
+    "handle different exceptions in Try block" in {
+      def failingFn(num: Int): String = {
+        num match {
+          case 0 => throw new RuntimeException("Number 0 is not allowed")
+          case n if n < 0 =>
+            throw new IllegalStateException("Number can't be negative")
+          case _ => "Success"
+        }
+      }
+
+      val triedResult = Try(failingFn(-3))
+      val handledResult = triedResult match {
+        case Success(value) => value
+        case Failure(exception) =>
+          exception match {
+            case is: IllegalStateException => "IllegalStateException"
+            case rt: RuntimeException      => "RuntimeException"
+            case _                         => "Unknown"
+          }
+      }
+      handledResult shouldBe "IllegalStateException"
+    }
+
     "catch all exceptions and errors using Throwable case block" in {
       def explodingFn: String =
         throw new LinkageError("Could not link the native library")
@@ -20,6 +65,22 @@ class NonFatalExceptionsTest extends AnyWordSpec with Matchers {
         }
       }
       exceptionHandlingFn shouldBe "Caught exception(LinkageError) from exception block"
+    }
+
+    "catch ControlThrowable using Throwable case block" in {
+      def explodingFn: String =
+        throw new ControlThrowable("This is not good!") {}
+
+      def exceptionHandlingFn = {
+        try {
+          explodingFn
+        } catch {
+          case _: ControlThrowable =>
+            "It is a bad practice to catch ControlThrowable"
+        }
+      }
+
+      exceptionHandlingFn shouldBe "It is a bad practice to catch ControlThrowable"
     }
 
     "not be able to catch LinkageError using Exception catch block" in {
@@ -42,23 +103,19 @@ class NonFatalExceptionsTest extends AnyWordSpec with Matchers {
   "Not be able to catch errors using Try block" in {
     def explodingFn: String =
       throw new LinkageError("Could not link the native library")
-
     def fnWithTry = {
       Try {
         explodingFn
       }
     }
-
     def tryToHandle = fnWithTry match {
       case Success(v)  => "success"
       case Failure(ex) => "Handled exception: " + ex.getClass.getSimpleName
     }
-
     // since it could not be handled, it should throw this linkage error.
     val intercepted = intercept[LinkageError](tryToHandle)
     // notice that it is not returning the message "Handled exception: LinkageError"
     intercepted.getMessage shouldBe "Could not link the native library"
-
   }
 
   "be able to catch NON-errors using Try block" in {
@@ -80,22 +137,25 @@ class NonFatalExceptionsTest extends AnyWordSpec with Matchers {
   }
 
   "handle non-errors using NonFatal block" in {
-    def explodingFn: String =
-      throw new Exception("Could not link the native library")
-    def fnWithTry = {
-      Try {
-        explodingFn
-      }
-    }
-
+    val fnWithTry: Try[String] =
+      Failure(new Exception("Something didn't work!"))
     val handled = fnWithTry.failed.get match {
       case NonFatal(nf) => "Handled only non-fatal exception here"
-      case _            => "Handled fatal exception: "
     }
     handled shouldBe "Handled only non-fatal exception here"
   }
 
-  "NOT be able to handle errors using NonFatal block" in {
+  "not able to handle errors using NonFatal block" in {
+    def result = try {
+      throw new OutOfMemoryError("No Memory available")
+    } catch {
+      case NonFatal(_) => "Handled NonFatal Exceptions"
+    }
+    val errorMsg = intercept[OutOfMemoryError](result).getMessage
+    errorMsg shouldBe "No Memory available"
+  }
+
+  "handle exceptions using NonFatal block" in {
     def explodingFn: String =
       throw new LinkageError("Could not link the native library")
 
@@ -110,5 +170,15 @@ class NonFatalExceptionsTest extends AnyWordSpec with Matchers {
 
     val intercepted = intercept[LinkageError](fnWithTry)
     intercepted.getMessage shouldBe "Could not link the native library"
+  }
+
+  "ignore fatal error from Try responsibility" in {
+    def handleWithTry = Try {
+      throw new VirtualMachineError(
+        "You are not strong enough to handle me!"
+      ) {}
+    }
+    val errorMsg = intercept[VirtualMachineError](handleWithTry).getMessage
+    errorMsg shouldBe "You are not strong enough to handle me!"
   }
 }
