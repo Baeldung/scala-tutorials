21570f243d3deaf4c46757f8a6e57e6ba14a8d51
Yadu Krishnan <yadavan88@gmail.com>
2023-06-17 09:25:47 +0200

    Updated skunk to latest and fixed warnings and errors

--- build.sbt
+++ build.sbt
@@ -349,7 +349,7 @@ lazy val scala_libraries_4 = (project in file("scala-libraries-4"))
   .configs(IntegrationTest)
   .settings(
     name := "scala-libraries-4",
-    scalaVersion := "2.13.10",
+    scalaVersion := scalaV,
     libraryDependencies += "com.lihaoyi" %% "utest" % "0.8.1" % "test",
     testFrameworks += new TestFramework("utest.runner.Framework"),
     libraryDependencies ++= scalaTestDeps
@@ -357,7 +357,7 @@ lazy val scala_libraries_4 = (project in file("scala-libraries-4"))
     libraryDependencies ++= Seq(
       "org.scala-lang.modules" %% "scala-async" % "1.0.1",
       scalaReflection % Provided,
-      "org.tpolecat" %% "skunk-core" % "0.5.1",
+      "org.tpolecat" %% "skunk-core" % "0.6.0",
       sparkSqlDep,
       sparkCoreDep,
       logback,
@@ -386,7 +386,7 @@ lazy val scala_libraries_4 = (project in file("scala-libraries-4"))
 lazy val scala_libraries_5 = (project in file("scala-libraries-5"))
   .settings(
     name := "scala-libraries-5",
-    scalaVersion := "2.13.10",
+    scalaVersion := scalaV,
     libraryDependencies ++= scalaTestDeps,
     libraryDependencies ++= Seq(
       sparkSqlDep,
--- scala-libraries-4/src/test/scala-2/com/baeldung/scala/skunk/Session.scala
+++ scala-libraries-4/src/test/scala-2/com/baeldung/scala/skunk/Session.scala
@@ -22,26 +22,26 @@ object Skunk {
     )
 
   def getAllUsers(resource: Resource[IO, Session[IO]]) = {
-    val query: Query[Void, Int ~ String ~ String ~ Int] =
+    val mappedQuery: Query[Void, User] =
       sql"SELECT * FROM Users"
-        .query(int4 ~ varchar(255) ~ varchar(255) ~ int4)
+        .query(int4 *: varchar(255) *: varchar(255) *: int4)
+        .to[User]
 
-    val mappedQuery: Query[Void, User] = query
-      .gmap[User]
     val results: IO[List[User]] = resource.use(s => s.execute(mappedQuery))
     results
   }
 
   def getUserWithId(resource: Resource[IO, Session[IO]]) = {
-    val query: Query[Int ~ String, User] =
+    val userDecoder: Decoder[User] =
+      (int4 *: varchar(255) *: varchar(255) *: int4).to[User]
+    val query: Query[(Int *: String *: EmptyTuple), User] =
       sql"""
         SELECT * FROM Users WHERE
           id = $int4 AND username LIKE $varchar
        """
-        .query(int4 ~ varchar(255) ~ varchar(255) ~ int4)
-        .gmap[User]
+        .query(userDecoder)
 
-    val preparedQuery: Resource[IO, PreparedQuery[IO, Int ~ String, User]] =
+    val preparedQuery =
       resource
         .flatMap(session => session.prepareR(query))
 
@@ -59,7 +59,7 @@ object Skunk {
   }
 
   def removePrepared(resource: Resource[IO, Session[IO]]) = {
-    val command: Command[Int ~ String] =
+    val command: Command[Int *: String *: EmptyTuple] =
       sql"""
         DELETE FROM Users WHERE
           id = $int4 and username = $varchar
