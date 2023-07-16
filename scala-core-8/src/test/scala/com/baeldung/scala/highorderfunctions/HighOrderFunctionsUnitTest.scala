163422c7968e5f8d79ce33831dd0e358ba42be00
Usman Mohyuddin <mr.usman76@gmail.com>
2023-07-16 22:33:44 +0500

    update tests to resolve conflict

--- scala-core-8/src/test/scala/com/baeldung/scala/highorderfunctions/HighOrderFunctionsUnitTest.scala
+++ scala-core-8/src/test/scala/com/baeldung/scala/highorderfunctions/HighOrderFunctionsUnitTest.scala
@@ -39,13 +39,6 @@ class HighOrderFunctionsUnitTest extends AnyFlatSpec with Matchers {
     assert(15 == actualSum)
   }
 
-  "reduceLeft" should "throw an exception" in {
-    val numbers = List.empty[Int]
-    assertThrows[UnsupportedOperationException] {
-      numbers.reduceLeft(_ max _)
-    }
-  }
-
   "foldLeft" should "calculate the sum correctly" in {
     val numbers = List(1, 2, 3, 4, 5)
     val actualSum = numbers.foldLeft(5)(_ + _)
@@ -70,12 +63,6 @@ class HighOrderFunctionsUnitTest extends AnyFlatSpec with Matchers {
     assert("ABCDE$" == actualResult)
   }
 
-  "foldRight" should "return the initial element i.e $" in {
-    val alphabets = List.empty[String]
-    val actualResult = alphabets.foldRight("$")(_ + _)
-    assert("$" == actualResult)
-  }
-
   "scanLeft" should "have correct intermediate states" in {
     val numbers = List(1, 2, 3, 4, 5)
     val actualResult = numbers.scanLeft(1)(_ + _)
@@ -86,6 +73,19 @@ class HighOrderFunctionsUnitTest extends AnyFlatSpec with Matchers {
     val numbers = List(1, 2, 3, 4, 5)
     val actualResult = numbers.scanRight(1)(_ + _)
     assert(List(16, 15, 13, 10, 6, 1) == actualResult)
+  }
+
+  "reduceLeft" should "throw an exception" in {
+    val numbers = List.empty[Int]
+    assertThrows[UnsupportedOperationException] {
+      numbers.reduceLeft(_ max _)
+    }
+  }
+
+  "foldRight" should "return the initial element i.e $" in {
+    val alphabets = List.empty[String]
+    val actualResult = alphabets.foldRight("$")(_ + _)
+    assert("$" == actualResult)
   }
 
   "scanRight" should "return the initial element i.e 5" in {
