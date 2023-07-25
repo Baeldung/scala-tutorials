4b1455508a15e8aac324f52571a2beb22d61c86d
Tom <tomsy0123@gmail.com>
2023-07-25 23:09:14 +0300

    Reformatted code #2

--- scala-core-8/src/test/scala-2/com/baeldung/scala/strings/CaseInsensitiveStringComparisonUnitTest.scala
+++ scala-core-8/src/test/scala-2/com/baeldung/scala/strings/CaseInsensitiveStringComparisonUnitTest.scala
@@ -4,17 +4,17 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class CaseInsensitiveStringComparisonUnitTest extends AnyFlatSpec with Matchers {
-
   "operating equals ignore case method on a string" should "return true if strings are the same when ignoring case" in {
     val str = "Hello World"
     val result = str.equalsIgnoreCase("hello world")
+
     result should be(true)
   }
 
   "operating toLowerCase.contains on a string" should "return if strings are the same when ignoring case" in {
     val str = "Hello World"
     val result = str.toLowerCase.contains("hello world".toLowerCase)
+
     result should be(true)
   }
-
 }
