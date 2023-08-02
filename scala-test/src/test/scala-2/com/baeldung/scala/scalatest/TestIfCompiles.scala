6af16797666a8b6e78be2a582ff81fd5d8875061
Mberic <51446308+Mberic@users.noreply.github.com>
2023-08-02 20:25:53 +0300

    Update TestIfCompiles.scala

--- scala-test/src/test/scala-2/com/baeldung/scala/scalatest/TestIfCompiles.scala
+++ scala-test/src/test/scala-2/com/baeldung/scala/scalatest/TestIfCompiles.scala
@@ -1,6 +1,7 @@
 package com.baeldung.scala.scalatest
 
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.flatspec._
+import org.scalatest.matchers.should.Matchers._
 
 class TestIfCompiles extends AnyFlatSpec {
 
