960e9d28376c05d9ba4291186b79e65f0962b094
Mberic <ericb55458@gmail.com>
2023-08-15 21:11:45 +0300

    Adding a IfCompilesTest file to new_branch

---
+++ scala-test/src/test/scala-2/com/baeldung/scala/scalatest/IfCompilesTest.scala
@@ -0,0 +1,12 @@
+package com.baeldung.scala.scalatest
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class IfCompilesTest extends AnyFlatSpec with Matchers {
+
+  "val x: Int = 258" shouldNot compile
+  "val x: Int = 2.0" shouldNot typeCheck
+  "val x: Int = 2" should compile
+}
+
