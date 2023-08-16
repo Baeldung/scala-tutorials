7907edebfe22d1295eb1eca2743d55b15744b067
Mberic <ericb55458@gmail.com>
2023-08-16 13:59:16 +0300

    Update IfCompilesTest file

--- scala-test/src/test/scala-2/com/baeldung/scala/scalatest/IfCompilesTest.scala
+++ scala-test/src/test/scala-2/com/baeldung/scala/scalatest/IfCompilesTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.matchers.should.Matchers
 
 class IfCompilesTest extends AnyFlatSpec with Matchers {
 
-  "val x: Int = 258" shouldNot compile
-  "val x: Int = 2.0" shouldNot typeCheck
-  "val x: Int = 2" should compile
+    "val x: Int = 2" should compile
+    // "val x: Int = 258" shouldNot compile
+    // "val x: Int = 2.0" shouldNot typeCheck
 }
