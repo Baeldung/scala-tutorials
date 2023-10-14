f39cc220d45e59333199bb0ae3f49ee636f61810
Bruno Unna <bruno.unna@gmail.com>
2023-10-14 20:02:47 +0100

    Ascertain covariance of IArray with cats and dogs

---
+++ scala3-lang-3/src/test/scala/com/baeldung/scala/immutablearrays/ImmutableArraySpec.scala
@@ -0,0 +1,23 @@
+package com.baeldung.scala.immutablearrays
+
+import org.scalatest.wordspec.AnyWordSpec
+
+class ImmutableArraySpec extends AnyWordSpec {
+  private trait Pet(name: String)
+  private case class Dog(name: String) extends Pet(name = name)
+  private case class Cat(name: String) extends Pet(name = name)
+
+  private val dogs: IArray[Dog] = IArray(Dog("champ"), Dog("barky"))
+  private val cats: IArray[Cat] = IArray(Cat("overlord"), Cat("silky"))
+
+  "dogs and cats" should {
+    "get along together" in {
+      val myPets: IArray[Pet] = cats ++ dogs
+      assertResult(4)(myPets.length)
+      assert(myPets.contains(Dog("barky")))
+      assert(myPets.contains(Cat("silky")))
+      assert(myPets.contains(Dog("champ")))
+      assert(myPets.contains(Cat("overlord")))
+    }
+  }
+}
