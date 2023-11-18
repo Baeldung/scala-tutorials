930039f4af97a14e7475cdc3e2a7d81137a40f97
Yadu Krishnan <yadavan88@gmail.com>
2023-11-18 10:54:44 +0100

    converted tests into table driven test

--- scala-core-modules/scala-core-8/src/main/scala/com/baeldung/scala/dates/DateListGenerator.scala
+++ scala-core-modules/scala-core-8/src/main/scala/com/baeldung/scala/dates/DateListGenerator.scala
@@ -6,7 +6,7 @@ import scala.annotation.tailrec
 
 object DateListGenerator {
 
-  def recursiveDateList(startDate: LocalDate, endDate: LocalDate) = {
+  def recursiveDateList(startDate: LocalDate, endDate: LocalDate): List[LocalDate] = {
     @tailrec
     def findNextDate(
       currentDate: LocalDate,
@@ -21,17 +21,31 @@ object DateListGenerator {
     findNextDate(startDate, Nil)
   }
 
-  def foldLeftDateList(startDate: LocalDate, endDate: LocalDate) = {
+  def foldLeftDateList(startDate: LocalDate, endDate: LocalDate): List[LocalDate] = {
     val noOfDays = ChronoUnit.DAYS.between(startDate, endDate) + 1
     (0 until noOfDays.toInt).foldLeft(List.empty[LocalDate]) { (acc, incr) =>
       acc :+ startDate.plusDays(incr)
     }
   }
 
-  def iteratorDateList(startDate: LocalDate, endDate: LocalDate) = {
+  def iteratorDateList(startDate: LocalDate, endDate: LocalDate): List[LocalDate] = {
     Iterator
       .iterate(startDate)(_.plusDays(1))
       .takeWhile(!_.isAfter(endDate))
       .toList
+  }
+
+  def tabulateDateList(startDate: LocalDate, endDate: LocalDate): List[LocalDate] = {
+    val noOfDays = ChronoUnit.DAYS.between(startDate, endDate) + 1
+    List.tabulate(noOfDays.toInt)(startDate.plusDays(_))
+  }
+
+  def dateListEpochDays(startDate: LocalDate, endDate: LocalDate): List[LocalDate]  = {
+    startDate.toEpochDay.to(endDate.toEpochDay).map(LocalDate.ofEpochDay).toList
+  }
+
+  def dateListDaysBetween(startDate: LocalDate, endDate: LocalDate): List[LocalDate]  = {
+    val noOfDays = ChronoUnit.DAYS.between(startDate, endDate) + 1
+    (0 until noOfDays.toInt).map(startDate.plusDays(_)).toList
   }
 }
