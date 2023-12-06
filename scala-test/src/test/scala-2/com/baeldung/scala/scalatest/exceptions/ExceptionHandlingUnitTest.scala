b8de0c98858fff71b9d3e2be171218c191029668
Yadu Krishnan <yadavan88@gmail.com>
2023-12-06 17:36:43 +0100

    Added the code sample for scalatest exception handling

--- scala-test/src/test/scala-2/com/baeldung/scala/scalatest/exceptions/ExceptionHandlingUnitTest.scala
+++ scala-test/src/test/scala-2/com/baeldung/scala/scalatest/exceptions/ExceptionHandlingUnitTest.scala
@@ -24,29 +24,33 @@ object ExceptionHandlingMethods {
 }
 
 class ExceptionHandlingUnitTest extends AsyncWordSpec with Matchers {
+  import ExceptionHandlingMethods._
 
   "Scalatest exception handler" should {
+
     "intercept thrown exception successfully" in {
-      val exception =
-        intercept[RuntimeException](ExceptionHandlingMethods.explodingMethod())
+      val exception = intercept[RuntimeException](explodingMethod())
       exception shouldBe a[RuntimeException]
       exception.getMessage shouldBe "Boom boom!"
     }
 
     "intercept thrown exception successfully using the parent type" in {
-      val exception =
-        intercept[Exception](ExceptionHandlingMethods.explodingMethod())
+      val exception = intercept[Exception](explodingMethod())
       exception shouldBe a[Exception]
       exception shouldBe a[RuntimeException]
       exception.getMessage shouldBe "Boom boom!"
     }
 
     "use assertThrows to handle thrown exception successfully" in {
-      assertThrows[RuntimeException](ExceptionHandlingMethods.explodingMethod())
+      assertThrows[RuntimeException](explodingMethod())
+    }
+
+    "use assertThrows to handle thrown exception successfully by using parent type" in {
+      assertThrows[Exception](explodingMethod())
     }
 
     "handle Try failures correctly" in {
-      val result = ExceptionHandlingMethods.getLastDigit(-100)
+      val result = getLastDigit(-100)
       result.failed.get shouldBe a[IllegalArgumentException]
       result.failed.get.getMessage should include(
         "Only positive numbers supported!"
@@ -54,7 +58,7 @@ class ExceptionHandlingUnitTest extends AsyncWordSpec with Matchers {
     }
 
     "handle future failures correctly" in {
-      val futureResult = ExceptionHandlingMethods.getDBResult()
+      val futureResult = getDBResult()
       futureResult.failed.map { ex =>
         ex shouldBe a[RuntimeException]
         ex.getMessage shouldBe "Unexpected error occurred!"
@@ -62,14 +66,14 @@ class ExceptionHandlingUnitTest extends AsyncWordSpec with Matchers {
     }
 
     "use recoverToExceptionIf method for futures" in {
-      val futureResult = ExceptionHandlingMethods.getDBResult()
+      val futureResult = getDBResult()
       recoverToExceptionIf[RuntimeException](futureResult).map { rt =>
         rt.getMessage shouldBe "Unexpected error occurred!"
       }
     }
 
     "use recoverToSucceededIf method for futures" in {
-      val futureResult = ExceptionHandlingMethods.getDBResult()
+      val futureResult = getDBResult()
       recoverToSucceededIf[RuntimeException](futureResult)
     }
   }
