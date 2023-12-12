c59da784c5d55544a263b45518be78a48de28f9e
Yadu Krishnan <yadavan88@gmail.com>
2023-12-12 19:00:36 +0100

    Scalafmt

--- scala-core-modules/scala-core-dates/src/main/scala/com/baeldung/scala/datesandtimes/JavaUtilDate.scala
+++ scala-core-modules/scala-core-dates/src/main/scala/com/baeldung/scala/datesandtimes/JavaUtilDate.scala
@@ -5,7 +5,8 @@ import scala.concurrent.{Await, ExecutionContext, Future}
 
 object JavaUtilDate extends App {
 
-  implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+  implicit val ec: ExecutionContext =
+    scala.concurrent.ExecutionContext.Implicits.global
 
   val format: ThreadLocal[SimpleDateFormat] =
     new ThreadLocal[SimpleDateFormat] {
