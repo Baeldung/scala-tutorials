7dc14b2505ce0f81a39452a5fa89f54bdc8234e9
Yadu Krishnan <yadavan88@gmail.com>
2023-12-18 20:14:36 +0100

    Reverted deletion of junit-5 module

---
+++ scala-test-junit5/src/test/scala-2/com/baeldung/junit5/ExampleTests.scala
@@ -0,0 +1,12 @@
+package com.baeldung.junit5
+
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+class ExampleTests {
+
+  @Disabled("Not implemented yet")
+  @Test
+  def testNotImplemented() {}
+
+}
