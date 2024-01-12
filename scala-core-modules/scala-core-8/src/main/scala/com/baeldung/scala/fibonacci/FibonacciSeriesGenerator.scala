7da1c1c6e5e21d4b99f191500c510bb0a746b9b8
Yadu Krishnan <yadavan88@gmail.com>
2024-01-12 18:28:10 +0100

    Added code and test for fibonacci series generation

---
+++ scala-core-modules/scala-core-8/src/main/scala/com/baeldung/scala/fibonacci/FibonacciSeriesGenerator.scala
@@ -0,0 +1,39 @@
+package com.baeldung.scala.fibonacci
+
+import scala.annotation.tailrec
+
+object FibonacciSeriesGenerator {
+
+  def fibSeriesRecursion(seriesSize: Int): Seq[Long] = {
+    def getNextNum(num: Long): Long = {
+      if (num <= 1) {
+        num
+      } else {
+        getNextNum(num - 1) + getNextNum(num - 2)
+      }
+    }
+    (0L until seriesSize).map(getNextNum)
+  }
+
+  def fibTailRec(seriesSize: Int): Seq[Long] = {
+    @tailrec
+    def fib(n: Int, a: Long, b: Long, acc: List[Long]): List[Long] = {
+      if (n <= 0) acc.reverse
+      else fib(n - 1, b, a + b, a :: acc)
+    }
+    fib(seriesSize, 0L, 1, Nil)
+  }
+
+  def fibLazyList(seriesSize: Int): Seq[Long] = {
+    lazy val fib: LazyList[Long] = 0L #:: 1L #:: fib.zip(fib.tail).map(_ + _)
+    fib.take(seriesSize).toList
+  }
+
+  def fibIterator(seriesSize: Int): List[Long] = {
+    Iterator
+      .iterate((0L, 1L)) { case (a, b) => (b, a + b) }
+      .map(_._1)
+      .take(seriesSize)
+      .toList
+  }
+}
