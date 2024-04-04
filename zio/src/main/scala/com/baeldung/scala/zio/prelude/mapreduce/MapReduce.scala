6d16e8496baf58ba8d8f27ef25b454c61f76cfff
Bruno Unna <bruno.unna@gmail.com>
2024-04-04 09:43:43 +0100

    Let scalafmt decide how my code should look like

--- zio/src/main/scala/com/baeldung/scala/zio/prelude/mapreduce/MapReduce.scala
+++ zio/src/main/scala/com/baeldung/scala/zio/prelude/mapreduce/MapReduce.scala
@@ -11,10 +11,13 @@ object MapReduce {
         left + right
     }
 
-  def mapReduce[A, B](as: List[A])(f: A => B)(implicit identity: Identity[B]): B =
+  def mapReduce[A, B](as: List[A])(f: A => B)(implicit
+    identity: Identity[B]
+  ): B =
     as.map(f).toNonEmptyList.map(_.reduce1).getOrElse(identity.identity)
 
-  def mapReduce2[F[+_] : ForEach, A, B: Identity](as: F[A])(f: A => B): B = as.foldMap(f)
+  def mapReduce2[F[+_]: ForEach, A, B: Identity](as: F[A])(f: A => B): B =
+    as.foldMap(f)
 
   private val wordsPerLine: String => Int = _.split(" ").length
 
@@ -22,6 +25,7 @@ object MapReduce {
 
   def wordCountAbstract(as: List[String]): Int = mapReduce(as)(wordsPerLine)
 
-  def wordCountVeryAbstract(as: List[String]): Int = mapReduce2(as)(wordsPerLine)
+  def wordCountVeryAbstract(as: List[String]): Int =
+    mapReduce2(as)(wordsPerLine)
 
 }
--- zio/src/main/scala/com/baeldung/scala/zio/prelude/typeclass/Printable.scala
+++ zio/src/main/scala/com/baeldung/scala/zio/prelude/typeclass/Printable.scala
@@ -1,32 +1,34 @@
 package com.baeldung.scala.zio.prelude.typeclass
 
-/**
- * Type class that instructs implementing types to be able to be formatted as strings.
- *
- * @tparam A type that needs to implement the trait
- */
+/** Type class that instructs implementing types to be able to be formatted as
+  * strings.
+  *
+  * @tparam A
+  *   type that needs to implement the trait
+  */
 trait Printable[A] {
-  /**
-   * Any type A that implements this trait must be able to present itself as a string.
-   *
-   * @param value instance of type [[A]] that implements the type class
-   * @return string representation of the parameter `value`
-   */
+
+  /** Any type A that implements this trait must be able to present itself as a
+    * string.
+    *
+    * @param value
+    *   instance of type [[A]] that implements the type class
+    * @return
+    *   string representation of the parameter `value`
+    */
   def format(value: A): String
 }
 
-/**
- * Provides implementations of the [[Printable]] type class for types [[String]] and [[Int]].
- */
+/** Provides implementations of the [[Printable]] type class for types
+  * [[String]] and [[Int]].
+  */
 object Printable {
 
-  /**
-   * Implementation of the [[Printable]] type class for [[String]].
-   */
+  /** Implementation of the [[Printable]] type class for [[String]].
+    */
   implicit val stringPrintable: Printable[String] = (value: String) => value
 
-  /**
-   * Implementation of the [[Printable]] type class for [[Int]].
-   */
+  /** Implementation of the [[Printable]] type class for [[Int]].
+    */
   implicit val intPrintable: Printable[Int] = (value: Int) => value.toString
 }
