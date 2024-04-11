bc5239e864f1a39c7444b95ab50584530692d5ca
Zeeshan Arif <zeesh.arif@gmail.com>
2024-04-11 10:36:48 +0500

    BAEL-704 Check whether strings are isomorphic or not (#1257)
    * BAEL-704 Check whether strings are isomorphic or not
    
    * BAEL-704 fixed formatting
    
    * BAEL-704 fixed formatting of unit test source file
    
    * BAEL-704 Added another method to check if strings are Isomorphic along with test

---
+++ scala-core-modules/scala-core-9/src/test/scala/com/baeldung/scala/isomorphic/IsomorphicStringsUnitTest.scala
@@ -0,0 +1,37 @@
+package com.baeldung.scala.isomorphic
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+import com.baeldung.scala.isomorphic.IsomorphicStringsChecker.*
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+class IsomorphicStringsUnitTest
+  extends AnyFlatSpec
+  with TableDrivenPropertyChecks
+  with Matchers {
+  private val isomorphicStringChecker = Seq(
+    ("isomorphicChecker", checkIsomorphicBothWays),
+    ("isomorphicChecker2", checkIsomorphic2BothWays)
+  )
+
+  private val table = Table(
+    ("str1", "str2", "Isomorphic"),
+    ("aab", "xxy", true),
+    ("aab", "xxyz", false),
+    ("aaba", "xxyz", false),
+    ("ab", "xxyz", false),
+    ("aabac", "xxyz", false),
+    ("aab", "xxb", true),
+    (" ", "x", true),
+    ("", "x", false),
+    ("aab", "xyy", false),
+    ("xxm", "aab", true)
+  )
+  isomorphicStringChecker.foreach { (name, fn) =>
+    it should s"Check Isomorphic two String using $name" in {
+      forAll(table) { (str1, str2, expectedResult) =>
+        fn(str1, str2) shouldBe expectedResult
+      }
+    }
+  }
+}
