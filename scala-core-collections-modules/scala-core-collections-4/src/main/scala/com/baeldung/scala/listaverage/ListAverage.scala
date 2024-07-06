47d7f73f04cd58081fa3bc21e76af17c3099da62
pedrorijo91 <pedrorijo91@gmail.com>
2024-07-06 08:38:01 +0100

    fix fmt

--- scala-core-collections-modules/scala-core-collections-4/src/main/scala/com/baeldung/scala/listaverage/ListAverage.scala
+++ scala-core-collections-modules/scala-core-collections-4/src/main/scala/com/baeldung/scala/listaverage/ListAverage.scala
@@ -2,19 +2,17 @@ package com.baeldung.scala.listaverage
 
 object ListAverage {
 
-
-  def naive(lst: List[Int]): Int  = {
+  def naive(lst: List[Int]): Int = {
     var total = 0
     var nrElements = 0
 
-    for(elem <- lst) {
+    for (elem <- lst) {
       total += elem
       nrElements += 1
     }
 
     total / nrElements
   }
-
 
   def averageDouble(lst: List[Int]): Double = {
     var total = 0
@@ -39,7 +37,8 @@ object ListAverage {
   }
 
   def averageWithFold(lst: List[Int]): Double = {
-    val (sum, size) = lst.foldLeft((0.0, 0))((pair, elem)=>(pair._1 + elem, pair._2 + 1))
+    val (sum, size) =
+      lst.foldLeft((0.0, 0))((pair, elem) => (pair._1 + elem, pair._2 + 1))
     sum / size
   }
 }
