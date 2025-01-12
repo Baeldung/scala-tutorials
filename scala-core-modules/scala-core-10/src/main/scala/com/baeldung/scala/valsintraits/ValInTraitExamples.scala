3b74d328450e7ec60ed1c11648cb7f8e6cb3767e
Callum Gibbons <callumgibbons@Callums-MacBook-Pro.local>
2025-01-12 20:29:52 +0000

    [SCALA-339] - Usage of val and def fields in traits

---
+++ scala-core-modules/scala-core-10/src/main/scala/com/baeldung/scala/valsintraits/ValInTraitExamples.scala
@@ -0,0 +1,39 @@
+package com.baeldung.scala.valsintraits
+
+import java.time.LocalDateTime
+
+object ValInTraitExamples {
+  trait CurrentTimePrinter {
+    val currentTime: LocalDateTime
+    def printCurrentTime(): Unit
+  }
+  class TimePrinterImpl extends CurrentTimePrinter {
+    override val currentTime: LocalDateTime = LocalDateTime.now()
+    override def printCurrentTime(): Unit = println(currentTime)
+  }
+
+  trait CurrentTimePrinterWithDef {
+    def currentTime: LocalDateTime
+    def printCurrentTime(): Unit
+  }
+  class TimePrinterWithDefImpl extends CurrentTimePrinterWithDef {
+    override def currentTime: LocalDateTime = LocalDateTime.now()
+    override def printCurrentTime(): Unit = println(currentTime)
+  }
+
+  trait TraitA {
+    val multiplier: Int
+    val result: Int = multiplier * 7
+  }
+  trait TraitB {
+    val multiplier: Int = 10
+  }
+  trait TraitAWithDef {
+    val multiplier: Int
+    def result: Int = multiplier * 7
+  }
+
+  object AB extends TraitA with TraitB
+  object BA extends TraitB with TraitA
+  object ABWithDef extends TraitAWithDef with TraitB
+}
